8a00ff2ef593bd3a604894260c4557c4bb5ab837
Jeremy Mikola <jmikola@gmail.com>
2014-05-13 17:11:33 -0400

    Use value objects and make QueryResult IteratorAggregate

--- docs/examples/insert.php
+++ docs/examples/insert.php
@@ -1,27 +1,26 @@
 <?php
 
-$batch = new \MongoDB\Batch\InsertBatch();
-$batch
+$ordered = true;
+
+$insertBatch = new \MongoDB\Batch\InsertBatch($ordered);
+$insertBatch
     ->add($hannes)
     ->add($hayley);
 
-$writeOptions = array(
-    "ordered" => true,
-    "writeConcern" => array(
-        "w" => 3,
-        "wtimeout" => 42,
-    ),
-);
+$w = 'majority';
+$w = 500;
+
+$writeConcern = new \MongoDB\WriteConcern($w, $wtimeout);
 
 $mm = new \MongoDB\Manager("mongodb://server1,server2/?replicaSet=FOOBAR");
 
-$results = $mm->executeWrite("db.collection", $batch, $writeOptions);
+$result = $mm->executeWrite("db.collection", $insertBatch, $writeConcern);
 
-assert($results instanceof \MongoDB\WriteResults);
+assert($result instanceof \MongoDB\WriteResult);
 
 printf(
     "Inserted %d documents %s in %d msec\n",
-    $results->getOpCount(),
-    $results->getServer()->getHostname(),
-    $results->getTimer()
+    $result->getOpCount(),
+    $result->getServer()->getHostname(),
+    $result->getTimer()
 );
--- docs/examples/query.php
+++ docs/examples/query.php
@@ -1,38 +1,33 @@
 <?php
 
-$queryDocument = array(
-    '$query'   => array("name" => "Hannes"),
-    '$orderBy' => array('name' => 1),
-    '$comment' => 'More special stuff',
+$criteria = array("name" => "Hannes");
+$selector = array('fieldname' => 1, 'fieldname.two' => 1);
+
+$query = new \MongoDB\Query($criteria, $selector);
+$query
+    ->setOrderBy(array('name' => 1))
+    ->setComment('More special stuff')
+    ->setLimit(-3) // Kill server cursor after 3 documents
+    ->setSkip(0)   // Don't skip any documents
+    ->setFlags(EXHAUST | NO_CURSOR_TIMEOUT);
+
+$tags = array(
+    array("dc:eu", "cpu:fast"),
+    array("dc:eu", "cc:is"),
+    array(""),
 );
 
-$queryFields = array(
-    'fieldname' => 1,
-    'fieldname.two' => 1,
-);
-
-$flags = EXHAUST | NO_CURSOR_TIMEOUT;
-$limit = -3; // Kill server cursor after 3 documents
-$skip = 0;   // Don't skip any documents
-
-$query = new \MongoDB\Query($queryDocument, $queryFields, $flags, $limit, $skip);
-
-$readPreference = array(
-    "type" => 'secondaryPreferred',
-    "tags" => array(
-        array("dc:eu", "cpu:fast"),
-        array("dc:eu", "cc:is"),
-        array(""),
-    ),
-);
+$readPreference = new \MongoDB\ReadPreference('secondaryPreferred', $tags);
 
 $mm = new \MongoDB\Manager("mongodb://server1,server2/?replicaSet=FOOBAR");
 
-$cursor = $mm->executeQuery("db.collection", $query, $readPreference);
+$result = $mm->executeQuery("db.collection", $query, $readPreference);
 
-assert($cursor instanceof \MongoDB\Cursor);
+assert($result instanceof \MongoDB\QueryResult);
+assert($result instanceof \IteratorAggregate);
+assert($result->getIterator() instanceof \MongoDB\Cursor);
 
-foreach($cursor as $person) {
+foreach($result as $person) {
     printf(
         "%s has lived in %s\n",
         $person->getName(),
