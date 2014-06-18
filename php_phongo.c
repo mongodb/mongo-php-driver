0b7e3b3bb10ba1eac624ba7a464d834714a6c950
Hannes Magnusson <bjori@mongodb.com>
2014-06-18 15:45:43 -0700

    PHP Extension CRUFT for phongo

---
+++ php_phongo.c
@@ -0,0 +1,211 @@
+/*
+  +---------------------------------------------------------------------------+
+  | PHP Driver for MongoDB                                                    |
+  +---------------------------------------------------------------------------+
+  | Copyright 2013-2014 MongoDB, Inc.                                         |
+  |                                                                           |
+  | Licensed under the Apache License, Version 2.0 (the "License");           |
+  | you may not use this file except in compliance with the License.          |
+  | You may obtain a copy of the License at                                   |
+  |                                                                           |
+  | http://www.apache.org/licenses/LICENSE-2.0                                |
+  |                                                                           |
+  | Unless required by applicable law or agreed to in writing, software       |
+  | distributed under the License is distributed on an "AS IS" BASIS,         |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  |
+  | See the License for the specific language governing permissions and       |
+  | limitations under the License.                                            |
+  +---------------------------------------------------------------------------+
+  | Copyright (c) 2014, MongoDB, Inc.                                         |
+  +---------------------------------------------------------------------------+
+*/
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+/* External libs */
+#include <bson.h>
+#include <mongoc.h>
+
+/* PHP Core stuff */
+#include <php.h>
+#include <php_ini.h>
+#include <php/ext/standard/info.h>
+#include "Zend/zend_interfaces.h"
+#include "ext/spl/spl_iterators.h"
+/* Our Compatability header */
+#include "php_compat_53.h"
+
+/* Our stuffz */
+#include "php_phongo.h"
+#include "php_bson.h"
+
+
+/* {{{ M[INIT|SHUTDOWN] R[INIT|SHUTDOWN] G[INIT|SHUTDOWN] MINFO INI */
+
+ZEND_DECLARE_MODULE_GLOBALS(phongo)
+/* {{{ INI entries */
+PHP_INI_BEGIN()
+    STD_PHP_INI_ENTRY((char *)"phongo.be_awesome", (char *)"yes", PHP_INI_ALL, OnUpdateString, be_awesome, zend_phongo_globals, phongo_globals)
+PHP_INI_END()
+/* }}} */
+
+/* {{{ PHP_GINIT_FUNCTION */
+PHP_GINIT_FUNCTION(phongo)
+{
+	phongo_globals->be_awesome = NULL;
+}
+/* }}} */
+
+/* {{{ PHP_MINIT_FUNCTION */
+PHP_MINIT_FUNCTION(phongo)
+{
+	(void)type; /* We don't care if we are loaded via dl() or extension= */
+
+	REGISTER_INI_ENTRIES();
+
+	/* Initialize libmongoc */
+	mongoc_init();
+
+	/* Prep default object handlers to be used when we register the classes */
+	/*
+	memcpy(&phongo_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	phongo_object_handlers.clone_obj = NULL;
+	phongo_object_handlers.read_property = NULL;
+	phongo_object_handlers.write_property = NULL;
+	phongo_object_handlers.get_debug_info = NULL;
+	*/
+
+	PHP_MINIT(bson)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(CursorId)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(GeneratedId)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(Cursor)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(CommandCursor)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(QueryCursor)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(WriteResult)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(DeleteResult)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(InsertResult)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(UpdateResult)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(CommandResult)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(WriteBatch)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(DeleteBatch)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(InsertBatch)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(UpdateBatch)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(Manager)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(Server)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(Command)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(Query)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(ReadPreference)(INIT_FUNC_ARGS_PASSTHRU);
+
+	PHP_MINIT(WriteConcernError)(INIT_FUNC_ARGS_PASSTHRU);
+	PHP_MINIT(WriteError)(INIT_FUNC_ARGS_PASSTHRU);
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_RINIT_FUNCTION */
+PHP_RINIT_FUNCTION(phongo)
+{
+	(void)type; /* We don't care if we are loaded via dl() or extension= */
+	(void)module_number; /* Really doesn't matter which module number we are */
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_RSHUTDOWN_FUNCTION */
+PHP_RSHUTDOWN_FUNCTION(phongo)
+{
+	(void)type; /* We don't care if we are loaded via dl() or extension= */
+	(void)module_number; /* Really doesn't matter which module number we are */
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION */
+PHP_MSHUTDOWN_FUNCTION(phongo)
+{
+	(void)type; /* We don't care if we are loaded via dl() or extension= */
+
+	/* Cleanup after libmongoc */
+	mongoc_cleanup();
+
+	UNREGISTER_INI_ENTRIES();
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_GSHUTDOWN_FUNCTION */
+PHP_GSHUTDOWN_FUNCTION(phongo)
+{
+	phongo_globals->be_awesome = NULL;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION */
+PHP_MINFO_FUNCTION(phongo)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "phongo support", "enabled");
+	php_info_print_table_row(2, "phongo", PHONGO_VERSION_S);
+	php_info_print_table_row(2, "libmongoc", MONGOC_VERSION_S);
+	php_info_print_table_row(2, "libbson", BSON_VERSION_S);
+	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES();
+}
+/* }}} */
+/* }}} */
+
+/* {{{ phongo_functions[]
+*/
+const zend_function_entry phongo_functions[] = {
+	PHP_FE(bson_encode,     NULL)
+	PHP_FE(bson_decode,     NULL)
+	PHP_FE(bson_to_json,    NULL)
+	PHP_FE_END
+};
+/* }}} */
+/* {{{ phongo_module_entry
+ */
+zend_module_entry phongo_module_entry = {
+	STANDARD_MODULE_HEADER,
+	"phongo",
+	phongo_functions,
+	PHP_MINIT(phongo),
+	PHP_MSHUTDOWN(phongo),
+	PHP_RINIT(phongo),
+	PHP_RSHUTDOWN(phongo),
+	PHP_MINFO(phongo),
+	PHONGO_VERSION,
+	PHP_MODULE_GLOBALS(phongo),
+	PHP_GINIT(phongo),
+	PHP_GSHUTDOWN(phongo),
+	NULL,
+	STANDARD_MODULE_PROPERTIES_EX
+};
+/* }}} */
+
+#ifdef COMPILE_DL_PHONGO
+ZEND_GET_MODULE(phongo)
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ php_phongo.h
@@ -0,0 +1,182 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2014 Hannes Magnusson                                  |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Hannes Magnusson <bjori@php.net>                             |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef PHONGO_H
+#define PHONGO_H
+
+extern zend_module_entry phongo_module_entry;
+
+#define PHONGO_VERSION_S "3.0.0"
+#define PHONGO_VERSION   PHONGO_VERSION_S
+
+#ifdef PHP_WIN32
+#	define PHONGO_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHONGO_API __attribute__ ((visibility("default")))
+#else
+#	define PHONGO_API
+#endif
+
+#ifdef ZTS
+#	include "TSRM.h"
+#endif
+
+ZEND_BEGIN_MODULE_GLOBALS(phongo)
+	char *be_awesome;
+ZEND_END_MODULE_GLOBALS(phongo)
+
+#ifdef ZTS
+#	define PHONGO_G(v) TSRMG(phongo_globals_id, zend_phongo_globals *, v)
+#else
+#	define PHONGO_G(v) (phongo_globals.v)
+#endif
+
+typedef struct {
+	zend_object std;
+} php_phongo_command_t;
+typedef struct {
+	zend_object std;
+} php_phongo_commandcursor_t;
+typedef struct {
+	zend_object std;
+} php_phongo_commandresult_t;
+typedef struct {
+	zend_object std;
+} php_phongo_cursor_t;
+typedef struct {
+	zend_object std;
+} php_phongo_cursorid_t;
+typedef struct {
+	zend_object std;
+} php_phongo_deletebatch_t;
+typedef struct {
+	zend_object std;
+} php_phongo_deleteresult_t;
+typedef struct {
+	zend_object std;
+} php_phongo_generatedid_t;
+typedef struct {
+	zend_object std;
+} php_phongo_insertbatch_t;
+typedef struct {
+	zend_object std;
+} php_phongo_insertresult_t;
+typedef struct {
+	zend_object std;
+} php_phongo_manager_t;
+typedef struct {
+	zend_object std;
+} php_phongo_query_t;
+typedef struct {
+	zend_object std;
+} php_phongo_querycursor_t;
+typedef struct {
+	zend_object std;
+} php_phongo_readpreference_t;
+typedef struct {
+	zend_object std;
+} php_phongo_server_t;
+typedef struct {
+	zend_object std;
+} php_phongo_updatebatch_t;
+typedef struct {
+	zend_object std;
+} php_phongo_updateresult_t;
+typedef struct {
+	zend_object std;
+} php_phongo_writebatch_t;
+typedef struct {
+	zend_object std;
+} php_phongo_writeconcernerror_t;
+typedef struct {
+	zend_object std;
+} php_phongo_writeerror_t;
+typedef struct {
+	zend_object std;
+} php_phongo_writeresult_t;
+
+extern PHPAPI zend_class_entry *php_phongo_command_ce;
+extern PHPAPI zend_class_entry *php_phongo_commandcursor_ce;
+extern PHPAPI zend_class_entry *php_phongo_commandresult_ce;
+extern PHPAPI zend_class_entry *php_phongo_cursor_ce;
+extern PHPAPI zend_class_entry *php_phongo_cursorid_ce;
+extern PHPAPI zend_class_entry *php_phongo_deletebatch_ce;
+extern PHPAPI zend_class_entry *php_phongo_deleteresult_ce;
+extern PHPAPI zend_class_entry *php_phongo_generatedid_ce;
+extern PHPAPI zend_class_entry *php_phongo_insertbatch_ce;
+extern PHPAPI zend_class_entry *php_phongo_insertresult_ce;
+extern PHPAPI zend_class_entry *php_phongo_manager_ce;
+extern PHPAPI zend_class_entry *php_phongo_query_ce;
+extern PHPAPI zend_class_entry *php_phongo_querycursor_ce;
+extern PHPAPI zend_class_entry *php_phongo_readpreference_ce;
+extern PHPAPI zend_class_entry *php_phongo_server_ce;
+extern PHPAPI zend_class_entry *php_phongo_updatebatch_ce;
+extern PHPAPI zend_class_entry *php_phongo_updateresult_ce;
+extern PHPAPI zend_class_entry *php_phongo_writebatch_ce;
+extern PHPAPI zend_class_entry *php_phongo_writeconcernerror_ce;
+extern PHPAPI zend_class_entry *php_phongo_writeerror_ce;
+extern PHPAPI zend_class_entry *php_phongo_writeresult_ce;
+
+typedef enum {
+	PHONGO_INVALID_ARGUMENT = 1,
+	PHONGO_SOMETHING_ELSE,
+} php_phongo_error_domain_t;
+
+#if 0
+PHPAPI zend_class_entry* phongo_exception_from_mongoc_domain(mongoc_error_domain_t domain);
+#endif
+PHPAPI zend_class_entry* phongo_exception_from_phongo_domain(php_phongo_error_domain_t domain);
+
+PHP_MINIT_FUNCTION(bson);
+PHP_MINIT_FUNCTION(Command);
+PHP_MINIT_FUNCTION(CommandCursor);
+PHP_MINIT_FUNCTION(CommandResult);
+PHP_MINIT_FUNCTION(Cursor);
+PHP_MINIT_FUNCTION(CursorId);
+PHP_MINIT_FUNCTION(DeleteBatch);
+PHP_MINIT_FUNCTION(DeleteResult);
+PHP_MINIT_FUNCTION(GeneratedId);
+PHP_MINIT_FUNCTION(InsertBatch);
+PHP_MINIT_FUNCTION(InsertResult);
+PHP_MINIT_FUNCTION(Manager);
+PHP_MINIT_FUNCTION(Query);
+PHP_MINIT_FUNCTION(QueryCursor);
+PHP_MINIT_FUNCTION(ReadPreference);
+PHP_MINIT_FUNCTION(Server);
+PHP_MINIT_FUNCTION(UpdateBatch);
+PHP_MINIT_FUNCTION(UpdateResult);
+PHP_MINIT_FUNCTION(WriteBatch);
+PHP_MINIT_FUNCTION(WriteConcernError);
+PHP_MINIT_FUNCTION(WriteError);
+PHP_MINIT_FUNCTION(WriteResult);
+PHP_FUNCTION(bson_encode);
+PHP_FUNCTION(bson_decode);
+PHP_FUNCTION(bson_to_json);
+#endif /* PHONGO_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
