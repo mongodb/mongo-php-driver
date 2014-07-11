96486491911005fe68e48edab5c52cdbf7d3ca45
Hannes Magnusson <bjori@mongodb.com>
2014-07-11 03:33:57 +0000

    pretend object_properties_init() exists on PHP5.3

--- php_compat_53.h
+++ php_compat_53.h
@@ -5,3 +5,8 @@
 #ifndef HASH_KEY_NON_EXISTENT
 # define HASH_KEY_NON_EXISTENT HASH_KEY_NON_EXISTANT
 #endif
+
+#if PHP_VERSION_ID < 50400
+# define object_properties_init(_std, _class_type) \
+		zend_hash_copy(_std.properties, &class_type->default_properties, (copy_ctor_func_t) zval_add_ref, NULL, sizeof(zval *));
+#endif
