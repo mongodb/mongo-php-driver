de35cf6cf08e70c802b172e88681a8d7834bedcb
Hannes Magnusson <bjori@mongodb.com>
2014-07-16 23:20:01 +0000

    I just learned about trap
    Automatically exit on failure

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+trap abort_on_failure ERR
+
+abort_on_failure () {
+	echo "Last command failed, exising"
+	exit 42;
+}
 
 MONGOC_VERSION=master
 LIBBSON_VERSION=master
 LCOV_VERSION=1.11
-
-
-abort_on_failure () {
-	if [ $1 -ne 0 ]; then
-		echo "Last command failed, exising"
-		exit 42;
-	fi
-}
 
 
 sudo apt-get install gdb
@@ -24,46 +22,34 @@ popd
 pushd src
 	if [ "$MONGOC_VERSION" = "master" ]; then
 		wget -O libmongoc.tar.gz https://github.com/mongodb/mongo-c-driver/archive/master.tar.gz
-		abort_on_failure $?
 	else
 		wget -O libmongoc.tar.gz https://github.com/mongodb/mongo-c-driver/releases/download/${MONGOC_VERSION}/mongo-c-driver-${MONGOC_VERSION}.tar.gz
-		abort_on_failure $?
 	fi
 
 
 	mkdir libmongoc
 	pushd libmongoc
 		tar zxf ../libmongoc.tar.gz --strip-components=1
-		abort_on_failure $?
 
 		pushd src
 			if [ "$LIBBSON_VERSION" = "master" ]; then
 				wget -O libbson.tar.gz https://github.com/mongodb/libbson/archive/master.tar.gz
-				abort_on_failure $?
 			else
 				wget -O libbson.tar.gz https://github.com/mongodb/libbson/releases/download/${LIBBSON_VERSION}/libbson-${LIBBSON_VERSION}.tar.gz
-				abort_on_failure $?
 			fi
 
 			pushd libbson
 				tar zxf ../libbson.tar.gz --strip-components=1
 
 				./autogen.sh --enable-debug --enable-debug-symbols=full --disable-ssl --enable-trace --quiet
-				abort_on_failure $?
 			popd
 
 		popd # src
 
 		./autogen.sh --enable-debug --enable-debug-symbols=full --with-libbson=bundled --disable-ssl --enable-trace --quiet
-		abort_on_failure $?
-
-		abort_on_failure $?
 
 		make -s -j2 all
-		abort_on_failure $?
-
 		sudo make install
-		abort_on_failure $?
 
 	popd # libmongoc
 popd # src
@@ -72,13 +58,8 @@ popd # src
 
 phpize
 ./configure --enable-coverage
-abort_on_failure $?
-
 make all
-abort_on_failure $?
-
 sudo make install
-abort_on_failure $?
 
 
 
