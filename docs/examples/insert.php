be82fbda80ec4af3c6f7a1415991c17185b4c39f
Hannes Magnusson <bjori@mongodb.com>
2014-07-19 02:52:24 +0000

    Improve example

--- docs/examples/insert.php
+++ docs/examples/insert.php
@@ -1,26 +1,25 @@
 <?php
+include "model.php";
 
 $ordered = true;
 
-$insertBatch = new \MongoDB\Batch\InsertBatch($ordered);
-$insertBatch
-    ->add($hannes)
-    ->add($hayley);
+$insertBatch = new \MongoDB\WriteBatch($ordered);
+$hannes_id = $insertBatch->insert($hannes);
+$hayley_id = $insertBatch->insert($hayley);
 
-$w = 'majority';
-$wtimeout = 500;
+$mm = new \MongoDB\Manager("mongodb://localhost:27017");
 
+$w = 1;
+$wtimeout = 1000;
 $writeConcern = new \MongoDB\WriteConcern($w, $wtimeout);
-
-$mm = new \MongoDB\Manager("mongodb://server1,server2/?replicaSet=FOOBAR");
-
 $result = $mm->executeWrite("db.collection", $insertBatch, $writeConcern);
 
 assert($result instanceof \MongoDB\WriteResult);
 
 printf(
-    "Inserted %d documents %s in %d msec\n",
-    $result->getOpCount(),
-    $result->getServer()->getHostname(),
-    $result->getTimer()
+    "Inserted %d documents to %s\n",
+    $result->getNumInserted(),
+    $result->getServer()->getHost()
 );
+printf("hannes: %s (%s)\nhayley: %s (%s)\n", $hannes_id, get_class($hannes_id), $hayley_id, get_class($hannes_id));
+
