e97a1e2a5f74f67076145681584d45d8ec566b3d
Hannes Magnusson <bjori@mongodb.com>
2015-01-07 09:02:37 -0800

    PHPC-131: Add jira issue collector

---
+++ docs/jira.js
@@ -0,0 +1,9 @@
+// Requires jQuery!
+jQuery.ajax({
+    url: "https://jira.mongodb.org/s/en_USbc7v3i-1988229788/6109/63/1.4.0-m3/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?collectorId=f84b3c75",
+    type: "get",
+    cache: true,
+    dataType: "script"
+});
+
+
