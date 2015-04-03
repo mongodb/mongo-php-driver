45bf77cb2d28e5e3224d64d52ea003a69766d7a3
Hannes Magnusson <bjori@php.net>
2015-04-03 12:36:02 -0700

    This apt-get update is slow -- no need to execute it all the time

--- scripts/ubuntu/essentials.sh
+++ scripts/ubuntu/essentials.sh
@@ -1,8 +1,10 @@
-# Tools you can't live without
-apt-get update
-apt-get install -y build-essential git vim
+if [ ! -e ".provisioned" ]; then
+	# Tools you can't live without
+	apt-get update
+	apt-get install -y build-essential git vim
 
-# I can't stand emacs
-echo 'set -o vi' | sudo tee /etc/profile.d/vishell.sh
+	# I can't stand emacs
+	echo 'set -o vi' | sudo tee /etc/profile.d/vishell.sh
 
-
+	touch .provisioned
+fi
