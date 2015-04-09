cb9c823b01db3d20b1fb2b4903b8afbf2dc84888
Hannes Magnusson <bjori@php.net>
2015-04-09 15:30:23 -0700

    Initial script to provision on Windows

---
+++ scripts/windows/compile.bat
@@ -0,0 +1,11 @@
+﻿cd C:\php-sdk\phpdev\vc11\x86\pecl
+"C:\Program Files\7-Zip\7z.exe" x -y Z:\phongo\mongodb-*.tgz
+"C:\Program Files\7-Zip\7z.exe" x -y mongodb-*.tar
+cd C:\php-sdk\phpdev\vc11\x86\php-src
+C:\php-sdk\bin\phpsdk_setvars.bat
+"c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+buildconf --force
+configure --disable-all --enable-cli --enable-json --with-openssl --enable-mongodb
+
+nmake
+#nmake test TESTS="..\pecl\mongodb-*\tests"
