58833def10f088c9401b925d80f9936e32f7e809
Hannes Magnusson <bjori@php.net>
2015-04-10 13:55:38 -0700

    Install ext/zlib so we can pecl install .tgz

--- scripts/freebsd/essentials.sh
+++ scripts/freebsd/essentials.sh
@@ -2,7 +2,7 @@
 sudo portsnap fetch extract update
 
 # Minimum required PHP install + pecl
-sudo pkg install -y pcre php56 php56-openssl php56-json pear autoconf pkgconf cyrus-sasl
+sudo pkg install -y pcre php56 php56-openssl php56-json php56-zlib pear autoconf pkgconf cyrus-sasl
 
 # We need vim. git requires curl. mongoc requires libtool and automake 
 sudo pkg install -y vim git curl libtool automake
