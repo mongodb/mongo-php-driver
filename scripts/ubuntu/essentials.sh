6b7081713a3666c02d7d3d95f2c63b246d581276
Hannes Magnusson <bjori@php.net>
2015-06-09 16:13:03 -0700

    install mongo-c-driver required building blocks

--- scripts/ubuntu/essentials.sh
+++ scripts/ubuntu/essentials.sh
@@ -1,7 +1,7 @@
 if [ ! -e ".provisioned" ]; then
 	# Tools you can't live without
 	apt-get update
-	apt-get install -y build-essential git vim
+	apt-get install -y build-essential git vim libtool autoconf
 
 	# I can't stand emacs
 	echo 'set -o vi' | sudo tee /etc/profile.d/vishell.sh
