8b347196308ad0e576fb36a392f6bc15070f5b55
Hannes Magnusson <bjori@php.net>
2015-08-25 09:28:55 -0700

    PHP7: Start a phongo_compat.c with things that shouldn't be done in headers

--- phongo_compat.h
+++ phongo_compat.h
@@ -1,3 +1,28 @@
+/*
+  +---------------------------------------------------------------------------+
+  | PHP Driver for MongoDB                                                    |
+  +---------------------------------------------------------------------------+
+  | Copyright 2013-2015 MongoDB, Inc.                                         |
+  |                                                                           |
+  | Licensed under the Apache License, Version 2.0 (the "License");           |
+  | you may not use this file except in compliance with the License.          |
+  | You may obtain a copy of the License at                                   |
+  |                                                                           |
+  | http://www.apache.org/licenses/LICENSE-2.0                                |
+  |                                                                           |
+  | Unless required by applicable law or agreed to in writing, software       |
+  | distributed under the License is distributed on an "AS IS" BASIS,         |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  |
+  | See the License for the specific language governing permissions and       |
+  | limitations under the License.                                            |
+  +---------------------------------------------------------------------------+
+  | Copyright (c) 2015 MongoDB, Inc.                                          |
+  +---------------------------------------------------------------------------+
+*/
+
+#ifndef PHONGO_COMPAT_H
+#define PHONGO_COMPAT_H
+
 #include <php.h>
 
 #ifdef PHP_WIN32
@@ -116,18 +141,25 @@
 
 #if PHP_VERSION_ID >= 70000
 # define SUPPRESS_UNUSED_WARNING(x)
-static inline void *x509_from_zval(zval *zval) {
-	return Z_RES_P(zval)->ptr;
-}
 #define DECLARE_RETURN_VALUE_USED int return_value_used = 1;
 #else
 # define SUPPRESS_UNUSED_WARNING(x) (void)x;
 # define DECLARE_RETURN_VALUE_USED
-static inline void *x509_from_zval(zval *zval TSRMLS_DC) {
-	int resource_type;
-	int type;
-
-	zend_list_find(Z_LVAL_P(zval), &resource_type);
-	return zend_fetch_resource(&zval TSRMLS_CC, -1, "OpenSSL X.509", &type, 1, resource_type);
-}
 #endif
+
+
+void *x509_from_zval(zval *zval TSRMLS_DC);
+void phongo_add_exception_prop(const char *prop, int prop_len, zval *value TSRMLS_DC);
+
+
+#endif /* PHONGO_COMPAT_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
