4c0f908c402770a5baf8e83e4a740ebbb05a84b5
Hannes Magnusson <bjori@php.net>
2015-08-27 16:22:46 -0700

    PHPC-383: Simplify getting the current exception zval*

--- phongo_compat.c
+++ phongo_compat.c
@@ -40,12 +40,11 @@ void *x509_from_zval(zval *zval TSRMLS_DC) {
 void phongo_add_exception_prop(const char *prop, int prop_len, zval *value TSRMLS_DC)
 {
 	if (EG(exception)) {
+		zval *ex = NULL;
+		EXCEPTION_P(EG(exception), ex);
 #if PHP_VERSION_ID >= 70000
-		zval ex;
-		ZVAL_OBJ(&ex, EG(exception));
-		zend_update_property(Z_OBJCE(ex), &ex, prop, prop_len, value);
+		zend_update_property(Z_OBJCE_P(ex), ex, prop, prop_len, value);
 #else
-		zval *ex = EG(exception);
 		zend_update_property(Z_OBJCE_P(ex), ex, prop, prop_len, value TSRMLS_CC);
 #endif
 
