18a437b9acaa956a361b53986b8e61e7507eb8aa
Derick Rethans <github@derickrethans.nl>
2015-12-02 18:32:28 +0000

    Added 3.2.0rc5 to our Mongo Orchestration suite

--- scripts/ubuntu/mongo-orchestration.sh
+++ scripts/ubuntu/mongo-orchestration.sh
@@ -1,5 +1,6 @@
 # Enable MongoDB Enterprise repo
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/testing multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-testing.list
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.0 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.0.list
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/2.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-2.6.list
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/2.4 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-2.4.list
@@ -7,14 +8,16 @@ apt-get update
 
 apt-get install -y libsnmp15 libgsasl7
 
+sudo apt-get download mongodb-enterprise-server=3.2.0~rc5
+sudo apt-get download mongodb-enterprise-mongos=3.2.0~rc5
 sudo apt-get download mongodb-enterprise-server=3.0.3
-sudo apt-get download mongodb-enterprise-mongos=3.0.3
 sudo apt-get download mongodb-enterprise-server=2.6.9
 sudo apt-get download mongodb-10gen-enterprise=2.4.13
 dpkg -x mongodb-10gen-enterprise_2.4.13_amd64.deb 2.4.13
 dpkg -x mongodb-enterprise-server_2.6.9_amd64.deb 2.6.9
 dpkg -x mongodb-enterprise-server_3.0.3_amd64.deb 3.0.3
-dpkg -x mongodb-enterprise-mongos_3.0.3_amd64.deb 3.0.3
+dpkg -x mongodb-enterprise-server_3.2.0~rc5_amd64.deb 3.2.0~rc5
+dpkg -x mongodb-enterprise-mongos_3.2.0~rc5_amd64.deb 3.2.0~rc5
 
 
 
