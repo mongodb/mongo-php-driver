bd22446623ce60bbecdb541b82a96fd95b634b7f
sandipmavani <mavanisandip94@gmail.com>
2016-08-30 12:39:26 +0530

    Update bson.c
    Bug Fix
    Got unsupported type 0 'unknown' #375

--- src/bson.c
+++ src/bson.c
@@ -1101,6 +1101,9 @@ try_again:
 		case IS_REFERENCE:
 			ZVAL_DEREF(entry);
 			goto try_again;
+		case IS_UNDEF:
+        		  bson_append_undefined(bson, key, key_len);
+             		break;	
 #endif
 
 		default:
