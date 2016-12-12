mongodb/mongo-php-driver

commit be58940cd9c1373dad97d0e4364e4b61881022b2

Ensure php_phongo_bson_visit_int64() has TSRMLS vars if needed
Fixes a 32-bit build error introduced in 22a39d68f3661438be6d6820faf508a5bc98d084 for PHPC-313

src/bson.c
@@ -530,7 +530,7 @@ bool php_phongo_bson_visit_int64(const bson_iter_t *iter ARG_UNUSED, const char 
 #else
 	zval *retval = ((php_phongo_bson_state *)data)->zchild;
 #endif
-
+	TSRMLS_FETCH();
 
 	ADD_ASSOC_INT64(retval, key, v_int64);
 

commit 9f0fedcb44da5efc41a5f36fc8f7dc6ecd3647d7

Only fetch TSRMLS for 32-bit platforms in int64 visitor
This fixes a build error introduced in be58940cd9c1373dad97d0e4364e4b61881022b2.

src/bson.c
@@ -530,7 +530,9 @@ bool php_phongo_bson_visit_int64(const bson_iter_t *iter ARG_UNUSED, const char 
 #else
 	zval *retval = ((php_phongo_bson_state *)data)->zchild;
 #endif
+#if SIZEOF_PHONGO_LONG == 4
 	TSRMLS_FETCH();
+#endif
 
 	ADD_ASSOC_INT64(retval, key, v_int64);
 