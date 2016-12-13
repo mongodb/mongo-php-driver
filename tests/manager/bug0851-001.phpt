mongodb/mongo-php-driver

commit 6d46d625770c56da4822ef4e4c7fb222f36703e2

PHPC-359: Ensure RP tag sets serialize as documents

php_phongo.c
@@ -823,6 +823,49 @@ void php_phongo_read_concern_to_zval(zval *retval, const mongoc_read_concern_t *
 	}
 } /* }}} */
 
+/* Prepare tagSets for BSON encoding by converting each array in the set to an
+ * object. This ensures that empty arrays will serialize as empty documents.
+ *
+ * php_phongo_read_preference_tags_are_valid() handles actual validation of the
+ * tag set structure. */
+void php_phongo_read_preference_prep_tagsets(zval *tagSets TSRMLS_DC) /* {{{ */
+{
+	HashTable     *ht_data;
+
+	if (Z_TYPE_P(tagSets) != IS_ARRAY) {
+		return;
+	}
+
+	ht_data = HASH_OF(tagSets);
+
+#if PHP_VERSION_ID >= 70000
+	{
+		zval *tagSet;
+
+		ZEND_HASH_FOREACH_VAL(ht_data, tagSet) {
+			if (Z_TYPE_P(tagSet) == IS_ARRAY) {
+				convert_to_object(tagSet);
+			}
+		} ZEND_HASH_FOREACH_END();
+	}
+#else
+	{
+		HashPosition   pos;
+		zval         **tagSet;
+
+		for (zend_hash_internal_pointer_reset_ex(ht_data, &pos);
+		     zend_hash_get_current_data_ex(ht_data, (void **) &tagSet, &pos) == SUCCESS;
+		     zend_hash_move_forward_ex(ht_data, &pos)) {
+			if (Z_TYPE_PP(tagSet) == IS_ARRAY) {
+				convert_to_object(*tagSet);
+			}
+		}
+	}
+#endif
+
+	return;
+} /* }}} */
+
 /* Checks if tags is valid to set on a mongoc_read_prefs_t. It may be null or an
  * array of one or more documents. */
 bool php_phongo_read_preference_tags_are_valid(const bson_t *tags) /* {{{ */
@@ -864,10 +907,10 @@ void php_phongo_read_preference_to_zval(zval *retval, const mongoc_read_prefs_t 
 	}
 
 	if (!bson_empty0(tags)) {
+		/* Use PHONGO_TYPEMAP_NATIVE_ARRAY for the root type since tags is an
+		 * array; however, inner documents and arrays can use the default. */
 		php_phongo_bson_state  state = PHONGO_BSON_STATE_INITIALIZER;
-		/* Use native arrays for debugging output */
 		state.map.root_type = PHONGO_TYPEMAP_NATIVE_ARRAY;
-		state.map.document_type = PHONGO_TYPEMAP_NATIVE_ARRAY;
 
 		phongo_bson_to_zval_ex(bson_get_data(tags), tags->len, &state);
 #if PHP_VERSION_ID >= 70000

src/MongoDB/Manager.c
@@ -100,6 +100,67 @@ static bool php_phongo_manager_merge_context_options(zval *zdriverOptions TSRMLS
 	return true;
 }
 
+/* Prepare tagSets for BSON encoding by converting each array in the set to an
+ * object. This ensures that empty arrays will serialize as empty documents.
+ *
+ * php_phongo_read_preference_tags_are_valid() handles actual validation of the
+ * tag set structure. */
+static void php_phongo_manager_prep_tagsets(zval *options TSRMLS_DC)
+{
+	HashTable     *ht_data;
+
+	if (Z_TYPE_P(options) != IS_ARRAY) {
+		return;
+	}
+
+	ht_data = HASH_OF(options);
+
+#if PHP_VERSION_ID >= 70000
+	{
+		zend_string *string_key = NULL;
+		zend_ulong   num_key = 0;
+		zval        *tagSets;
+
+		ZEND_HASH_FOREACH_KEY_VAL(ht_data, num_key, string_key, tagSets) {
+			if (!string_key) {
+				continue;
+			}
+
+			/* php_phongo_make_uri() and php_phongo_apply_rp_options_to_uri()
+			 * are both case-insensitive, so we need to be as well. */
+			if (!strcasecmp(ZSTR_VAL(string_key), "readpreferencetags")) {
+				php_phongo_read_preference_prep_tagsets(tagSets TSRMLS_CC);
+			}
+		} ZEND_HASH_FOREACH_END();
+	}
+#else
+	{
+		HashPosition   pos;
+		zval         **tagSets;
+
+		for (zend_hash_internal_pointer_reset_ex(ht_data, &pos);
+		     zend_hash_get_current_data_ex(ht_data, (void **) &tagSets, &pos) == SUCCESS;
+		     zend_hash_move_forward_ex(ht_data, &pos)) {
+			char  *string_key = NULL;
+			uint   string_key_len = 0;
+			ulong  num_key = 0;
+
+			if (HASH_KEY_IS_STRING != zend_hash_get_current_key_ex(ht_data, &string_key, &string_key_len, &num_key, 0, &pos)) {
+				continue;
+			}
+
+			/* php_phongo_make_uri() and php_phongo_apply_rp_options_to_uri()
+			 * are both case-insensitive, so we need to be as well. */
+			if (!strcasecmp(string_key, "readpreferencetags")) {
+				php_phongo_read_preference_prep_tagsets(*tagSets TSRMLS_CC);
+			}
+		}
+	}
+#endif
+
+	return;
+} /* }}} */
+
 /* {{{ proto void Manager::__construct([string $uri = "mongodb://127.0.0.1/"[, array $options = array()[, array $driverOptions = array()]]])
    Constructs a new Manager */
 PHP_METHOD(Manager, __construct)
@@ -123,6 +184,10 @@ PHP_METHOD(Manager, __construct)
 		return;
 	}
 	zend_restore_error_handling(&error_handling TSRMLS_CC);
+
+	if (options) {
+		php_phongo_manager_prep_tagsets(options TSRMLS_CC);
+	}
 
 	if (driverOptions && !php_phongo_manager_merge_context_options(driverOptions TSRMLS_CC)) {
 		/* Exception should already have been thrown */

src/MongoDB/ReadPreference.c
@@ -86,6 +86,7 @@ PHP_METHOD(ReadPreference, __construct)
 	if (tagSets) {
 		bson_t *tags = bson_new();
 
+		php_phongo_read_preference_prep_tagsets(tagSets TSRMLS_CC);
 		phongo_zval_to_bson(tagSets, PHONGO_BSON_NONE, (bson_t *)tags, NULL TSRMLS_CC);
 
 		if (!php_phongo_read_preference_tags_are_valid(tags)) {

php_phongo.h
@@ -128,6 +128,7 @@ const mongoc_write_concern_t* phongo_write_concern_from_zval  (zval *zwrite_conc
 
 php_phongo_server_description_type_t php_phongo_server_description_type(mongoc_server_description_t *sd);
 
+void php_phongo_read_preference_prep_tagsets(zval *tagSets TSRMLS_DC);
 bool php_phongo_read_preference_tags_are_valid(const bson_t *tags);
 
 void php_phongo_server_to_zval(zval *retval, mongoc_server_description_t *sd);

tests/manager/manager-ctor-read_preference-001.phpt
@@ -33,12 +33,12 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(2) {
     [0]=>
-    array(1) {
+    object(stdClass)#%d (%d) {
       ["tag"]=>
       string(3) "one"
     }
     [1]=>
-    array(0) {
+    object(stdClass)#%d (%d) {
     }
   }
 }
@@ -58,12 +58,12 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(2) {
     [0]=>
-    array(1) {
+    object(stdClass)#%d (%d) {
       ["tag"]=>
       string(3) "one"
     }
     [1]=>
-    array(0) {
+    object(stdClass)#%d (%d) {
     }
   }
 }

tests/manager/manager-ctor-read_preference-003.phpt
@@ -29,7 +29,7 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(1) {
     [0]=>
-    array(1) {
+    object(stdClass)#%d (%d) {
       ["tag"]=>
       string(3) "one"
     }

tests/manager/manager-getreadpreference-001.phpt
@@ -52,14 +52,14 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(2) {
     [0]=>
-    array(2) {
+    object(stdClass)#%d (%d) {
       ["dc"]=>
       string(2) "ny"
       ["use"]=>
       string(7) "reports"
     }
     [1]=>
-    array(0) {
+    object(stdClass)#%d (%d) {
     }
   }
 }
@@ -69,14 +69,14 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(2) {
     [0]=>
-    array(2) {
+    object(stdClass)#%d (%d) {
       ["dc"]=>
       string(2) "ny"
       ["use"]=>
       string(7) "reports"
     }
     [1]=>
-    array(0) {
+    object(stdClass)#%d (%d) {
     }
   }
 }
@@ -86,7 +86,7 @@ object(MongoDB\Driver\ReadPreference)#%d (%d) {
   ["tags"]=>
   array(1) {
     [0]=>
-    array(1) {
+    object(stdClass)#%d (%d) {
       ["dc"]=>
       string(2) "ca"
     }

tests/readConcern/readconcern-constants.phpt
@@ -6,6 +6,7 @@ MongoDB\Driver\ReadConcern constants
 <?php
 require_once __DIR__ . "/../utils/basic.inc";
 
+var_dump(MongoDB\Driver\ReadConcern::LINEARIZABLE);
 var_dump(MongoDB\Driver\ReadConcern::LOCAL);
 var_dump(MongoDB\Driver\ReadConcern::MAJORITY);
 
@@ -13,6 +14,7 @@ var_dump(MongoDB\Driver\ReadConcern::MAJORITY);
 ===DONE===
 <?php exit(0); ?>
 --EXPECTF--
+string(12) "linearizable"
 string(5) "local"
 string(8) "majority"
 ===DONE===

commit 120b270377aef4836c2cc46cd6dc69fdc714cebd

PHPC-851: Separate argument zvals before modification
This primarily fixes a bug introduced in 6d46d625770c56da4822ef4e4c7fb222f36703e2 for PHPC-359, since we convert tag sets from arrays to objects to ensure proper BSON serialization. Not only was the array mutation visible to calling contexts, but a crash could occur if the array was immutable due to OPcache.

For Manager's $driverOptions, a comment in 1060cb8ba4202461ef57fde3ab768fe790acb056 for PHPC-433 indicated that we should have separated its zval due to possible modification, but the appropriate zend_parse_parameters() flag was never used.

php_phongo.c
@@ -837,7 +837,9 @@ void php_phongo_read_preference_prep_tagsets(zval *tagSets TSRMLS_DC) /* {{{ */
 		zval *tagSet;
 
 		ZEND_HASH_FOREACH_VAL(ht_data, tagSet) {
+			ZVAL_DEREF(tagSet);
 			if (Z_TYPE_P(tagSet) == IS_ARRAY) {
+				SEPARATE_ZVAL_NOREF(tagSet);
 				convert_to_object(tagSet);
 			}
 		} ZEND_HASH_FOREACH_END();
@@ -851,6 +853,7 @@ void php_phongo_read_preference_prep_tagsets(zval *tagSets TSRMLS_DC) /* {{{ */
 		     zend_hash_get_current_data_ex(ht_data, (void **) &tagSet, &pos) == SUCCESS;
 		     zend_hash_move_forward_ex(ht_data, &pos)) {
 			if (Z_TYPE_PP(tagSet) == IS_ARRAY) {
+				SEPARATE_ZVAL_IF_NOT_REF(tagSet);
 				convert_to_object(*tagSet);
 			}
 		}

src/MongoDB/Manager.c
@@ -129,6 +129,8 @@ static void php_phongo_manager_prep_tagsets(zval *options TSRMLS_DC)
 			/* php_phongo_make_uri() and php_phongo_apply_rp_options_to_uri()
 			 * are both case-insensitive, so we need to be as well. */
 			if (!strcasecmp(ZSTR_VAL(string_key), "readpreferencetags")) {
+				ZVAL_DEREF(tagSets);
+				SEPARATE_ZVAL_NOREF(tagSets);
 				php_phongo_read_preference_prep_tagsets(tagSets TSRMLS_CC);
 			}
 		} ZEND_HASH_FOREACH_END();
@@ -152,6 +154,7 @@ static void php_phongo_manager_prep_tagsets(zval *options TSRMLS_DC)
 			/* php_phongo_make_uri() and php_phongo_apply_rp_options_to_uri()
 			 * are both case-insensitive, so we need to be as well. */
 			if (!strcasecmp(string_key, "readpreferencetags")) {
+				SEPARATE_ZVAL_IF_NOT_REF(tagSets);
 				php_phongo_read_preference_prep_tagsets(*tagSets TSRMLS_CC);
 			}
 		}
@@ -177,9 +180,10 @@ PHP_METHOD(Manager, __construct)
 	zend_replace_error_handling(EH_THROW, phongo_exception_from_phongo_domain(PHONGO_ERROR_INVALID_ARGUMENT), &error_handling TSRMLS_CC);
 	intern = Z_MANAGER_OBJ_P(getThis());
 
-	/* Separate the driverOptions zval, since we may end up modifying it in
-	 * php_phongo_manager_merge_context_options() below. */
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|s!a!a!", &uri_string, &uri_string_len, &options, &driverOptions) == FAILURE) {
+	/* Separate the options and driverOptions zvals, since we may end up
+	 * modifying them in php_phongo_manager_prep_tagsets() and
+	 * php_phongo_manager_merge_context_options() below, respectively. */
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|s!a/!a/!", &uri_string, &uri_string_len, &options, &driverOptions) == FAILURE) {
 		zend_restore_error_handling(&error_handling TSRMLS_CC);
 		return;
 	}

src/MongoDB/ReadPreference.c
@@ -63,7 +63,9 @@ PHP_METHOD(ReadPreference, __construct)
 	zend_replace_error_handling(EH_THROW, phongo_exception_from_phongo_domain(PHONGO_ERROR_INVALID_ARGUMENT), &error_handling TSRMLS_CC);
 	intern = Z_READPREFERENCE_OBJ_P(getThis());
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l|a!a!", &mode, &tagSets, &options) == FAILURE) {
+	/* Separate the tagSets zval, since we may end up modifying it in
+	 * php_phongo_read_preference_prep_tagsets() below. */
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l|a/!a!", &mode, &tagSets, &options) == FAILURE) {
 		zend_restore_error_handling(&error_handling TSRMLS_CC);
 		return;
 	}

tests/manager/bug0851-001.phpt
@@ -1,0 +1,55 @@
+--TEST--
+PHPC-851: Manager constructor should not modify options argument
+--FILE--
+<?php
+
+$options = [
+    'readPreference' => 'secondaryPreferred',
+    'readPreferenceTags' => [
+        ['dc' => 'ny'],
+        [],
+    ],
+];
+
+$manager = new MongoDB\Driver\Manager(null, $options);
+var_dump($options);
+
+/* Dump the Manager's ReadPreference to ensure that each element in the
+ * readPreferenceTags option was converted to an object. */
+var_dump($manager->getReadPreference());
+
+?>
+===DONE===
+<?php exit(0); ?>
+--EXPECTF--
+array(2) {
+  ["readPreference"]=>
+  string(18) "secondaryPreferred"
+  ["readPreferenceTags"]=>
+  array(2) {
+    [0]=>
+    array(1) {
+      ["dc"]=>
+      string(2) "ny"
+    }
+    [1]=>
+    array(0) {
+    }
+  }
+}
+object(MongoDB\Driver\ReadPreference)#%d (%d) {
+  ["mode"]=>
+  string(18) "secondaryPreferred"
+  ["tags"]=>
+  array(2) {
+    [0]=>
+    object(stdClass)#%d (%d) {
+      ["dc"]=>
+      string(2) "ny"
+    }
+    [1]=>
+    object(stdClass)#%d (%d) {
+    }
+  }
+}
+===DONE===

tests/manager/bug0851-002.phpt
@@ -1,0 +1,28 @@
+--TEST--
+PHPC-851: Manager constructor should not modify driverOptions argument
+--FILE--
+<?php
+
+$driverOptions = [
+    'weak_cert_validation' => true,
+    'context' => stream_context_create([
+        'ssl' => [
+            'allow_self_signed' => true,
+        ],
+    ]),
+];
+
+$manager = new MongoDB\Driver\Manager(null, [], $driverOptions);
+var_dump($driverOptions);
+
+?>
+===DONE===
+<?php exit(0); ?>
+--EXPECT--
+array(2) {
+  ["weak_cert_validation"]=>
+  bool(true)
+  ["context"]=>
+  resource(4) of type (stream-context)
+}
+===DONE===

tests/readPreference/bug0851-001.phpt
@@ -1,0 +1,47 @@
+--TEST--
+PHPC-851: ReadPreference constructor should not modify tagSets argument
+--FILE--
+<?php
+
+$tagSets = [
+    ['dc' => 'ny'],
+    [],
+];
+
+$rp = new MongoDB\Driver\ReadPreference(MongoDB\Driver\ReadPreference::RP_SECONDARY_PREFERRED, $tagSets);
+var_dump($tagSets);
+
+/* Dump the Manager's ReadPreference to ensure that each element in the $tagSets
+ * argument was converted to an object. */
+var_dump($rp);
+
+?>
+===DONE===
+<?php exit(0); ?>
+--EXPECTF--
+array(2) {
+  [0]=>
+  array(1) {
+    ["dc"]=>
+    string(2) "ny"
+  }
+  [1]=>
+  array(0) {
+  }
+}
+object(MongoDB\Driver\ReadPreference)#%d (%d) {
+  ["mode"]=>
+  string(18) "secondaryPreferred"
+  ["tags"]=>
+  array(2) {
+    [0]=>
+    object(stdClass)#%d (%d) {
+      ["dc"]=>
+      string(2) "ny"
+    }
+    [1]=>
+    object(stdClass)#%d (%d) {
+    }
+  }
+}
+===DONE===