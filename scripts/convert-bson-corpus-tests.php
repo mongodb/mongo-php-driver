0fd4ab425884d0f44e7f675e0c82168b16ee98fe
Jeremy Mikola <jmikola@gmail.com>
2017-07-10 17:18:10 -0400

    PHPC-977: Update BSON corpous tests
    Generated from mongodb/specifications@7b5793561ec9b0d73b8af04c28002788b30d0141

--- scripts/convert-bson-corpus-tests.php
+++ scripts/convert-bson-corpus-tests.php
@@ -21,6 +21,11 @@ foreach (array_slice($argv, 1) as $inputFile) {
         continue;
     }
 
+    if ( ! isset($test['description'])) {
+        printf("Skipping test file without \"description\" field: %s\n", $inputFile);
+        continue;
+    }
+
     if ( ! empty($test['deprecated'])) {
         printf("Skipping deprecated test file: %s\n", $inputFile);
         continue;
@@ -29,7 +34,12 @@ foreach (array_slice($argv, 1) as $inputFile) {
     if ( ! empty($test['valid'])) {
         foreach ($test['valid'] as $i => $case) {
             $outputFile = sprintf('%s-valid-%03d.phpt', pathinfo($inputFile, PATHINFO_FILENAME), $i + 1);
-            $output = renderPhpt(getParamsForValid($test, $case));
+            try {
+                $output = renderPhpt(getParamsForValid($test, $case));
+            } catch (Exception $e) {
+                printf("Error processing valid[%d] in %s: %s\n", $i, $inputFile, $e->getMessage());
+                continue;
+            }
 
             if (false === file_put_contents($outputPath . '/' . $outputFile, $output)) {
                 printf("Error writing valid[%d] in %s\n", $i, $inputFile);
@@ -41,7 +51,12 @@ foreach (array_slice($argv, 1) as $inputFile) {
     if ( ! empty($test['decodeErrors'])) {
         foreach ($test['decodeErrors'] as $i => $case) {
             $outputFile = sprintf('%s-decodeError-%03d.phpt', pathinfo($inputFile, PATHINFO_FILENAME), $i + 1);
-            $output = renderPhpt(getParamsForDecodeError($test, $case));
+            try {
+                $output = renderPhpt(getParamsForDecodeError($test, $case));
+            } catch (Exception $e) {
+                printf("Error processing decodeErrors[%d] in %s: %s\n", $i, $inputFile, $e->getMessage());
+                continue;
+            }
 
             if (false === file_put_contents($outputPath . '/' . $outputFile, $output)) {
                 printf("Error writing decodeErrors[%d] in %s\n", $i, $inputFile);
@@ -55,8 +70,8 @@ foreach (array_slice($argv, 1) as $inputFile) {
             $outputFile = sprintf('%s-parseError-%03d.phpt', pathinfo($inputFile, PATHINFO_FILENAME), $i + 1);
             try {
                 $output = renderPhpt(getParamsForParseError($test, $case));
-            } catch (UnexpectedValueException $e) {
-                printf("Parse errors not supported for BSON type: %s\n", $test['bson_type']);
+            } catch (Exception $e) {
+                printf("Error processing parseErrors[%d] in %s: %s\n", $i, $inputFile, $e->getMessage());
                 continue;
             }
 
@@ -70,65 +85,101 @@ foreach (array_slice($argv, 1) as $inputFile) {
 
 function getParamsForValid(array $test, array $case)
 {
+    foreach (['description', 'canonical_bson', 'canonical_extjson'] as $field) {
+        if (!isset($case[$field])) {
+            throw new InvalidArgumentException(sprintf('Missing "%s" field', $field));
+        }
+    }
+
     $code = '';
     $expect = '';
 
-    $bson = $case['bson'];
-    $canonicalBson = isset($case['canonical_bson']) ? $case['canonical_bson'] : $bson;
+    $lossy = isset($case['lossy']) ? (boolean) $case['lossy'] : false;
+
+    $canonicalBson = $case['canonical_bson'];
     $expectedCanonicalBson = strtolower($canonicalBson);
+    $code .= sprintf('$canonicalBson = hex2bin(%s);', var_export($canonicalBson, true)) . "\n";
 
-    $code .= sprintf('$bson = hex2bin(%s);', var_export($bson, true)) . "\n";
+    if (isset($case['degenerate_bson'])) {
+        $degenerateBson = $case['degenerate_bson'];
+        $expectedDegenerateBson = strtolower($degenerateBson);
+        $code .= sprintf('$degenerateBson = hex2bin(%s);', var_export($degenerateBson, true)) . "\n";
+    }
 
-    $code .= "\n// BSON to Canonical BSON\n";
-    $code .= 'echo bin2hex(fromPHP(toPHP($bson))), "\n";' . "\n";
+    if (isset($case['converted_bson'])) {
+        $convertedBson = $case['converted_bson'];
+        $expectedConvertedBson = strtolower($convertedBson);
+        $code .= sprintf('$convertedBson = hex2bin(%s);', var_export($convertedBson, true)) . "\n";
+    }
+
+    $canonicalExtJson = $case['canonical_extjson'];
+    $expectedCanonicalExtJson = json_canonicalize($canonicalExtJson);
+    $code .= sprintf('$canonicalExtJson = %s;', var_export($canonicalExtJson, true)) . "\n";
+
+    if (isset($case['relaxed_extjson'])) {
+        $relaxedExtJson = $case['relaxed_extjson'];
+        $expectedRelaxedExtJson = json_canonicalize($relaxedExtJson);
+        $code .= sprintf('$relaxedExtJson = %s;', var_export($relaxedExtJson, true)) . "\n";
+    }
+
+    if (isset($case['degenerate_extjson'])) {
+        $degenerateExtJson = $case['degenerate_extjson'];
+        $expectedDegenerateExtJson = json_canonicalize($degenerateExtJson);
+        $code .= sprintf('$degenerateExtJson = %s;', var_export($degenerateExtJson, true)) . "\n";
+    }
+
+    if (isset($case['converted_extjson'])) {
+        $convertedExtJson = $case['converted_extjson'];
+        $expectedConvertedExtJson = json_canonicalize($convertedExtJson);
+        $code .= sprintf('$convertedExtJson = %s;', var_export($convertedExtJson, true)) . "\n";
+    }
+
+    $code .= "\n// Canonical BSON -> Native -> Canonical BSON \n";
+    $code .= 'echo bin2hex(fromPHP(toPHP($canonicalBson))), "\n";' . "\n";
     $expect .= $expectedCanonicalBson . "\n";
 
-    if ($bson !== $canonicalBson) {
-        $code .= "\n" . sprintf('$canonicalBson = hex2bin(%s);', var_export($canonicalBson, true)) . "\n";
-        $code .= "\n// Canonical BSON to Canonical BSON\n";
-        $code .= 'echo bin2hex(fromPHP(toPHP($canonicalBson))), "\n";' . "\n";
+    $code .= "\n// Canonical BSON -> Canonical extJSON \n";
+    $code .= 'echo json_canonicalize(toExtendedJSON($canonicalBson)), "\n";' . "\n";;
+    $expect .= $expectedCanonicalExtJson . "\n";
+
+    if (isset($relaxedExtJson)) {
+        $code .= "\n// Canonical BSON -> Relaxed extJSON \n";
+        $code .= 'echo json_canonicalize(toJSON($canonicalBson)), "\n";' . "\n";;
+        $expect .= $expectedRelaxedExtJson . "\n";
+    }
+
+    if (!$lossy) {
+        $code .= "\n// Canonical extJSON -> Canonical BSON \n";
+        $code .= 'echo bin2hex(fromJSON($canonicalExtJson)), "\n";' . "\n";
         $expect .= $expectedCanonicalBson . "\n";
     }
 
-    if (isset($case['extjson'])) {
-        $json = $case['extjson'];
-        $canonicalJson = isset($case['canonical_extjson']) ? $case['canonical_extjson'] : $json;
-        $expectedCanonicalJson = json_canonicalize($canonicalJson);
-        $lossy = isset($case['lossy']) ? (boolean) $case['lossy'] : false;
+    if (isset($degenerateBson)) {
+        $code .= "\n// Degenerate BSON -> Native -> Canonical BSON \n";
+        $code .= 'echo bin2hex(fromPHP(toPHP($degenerateBson))), "\n";' . "\n";
+        $expect .= $expectedCanonicalBson . "\n";
 
-        $code .= "\n// BSON to Canonical extJSON\n";
-        $code .= 'echo json_canonicalize(toExtendedJSON($bson)), "\n";' . "\n";;
-        $expect .= $expectedCanonicalJson . "\n";
+        $code .= "\n// Degenerate BSON -> Canonical extJSON \n";
+        $code .= 'echo json_canonicalize(toExtendedJSON($degenerateBson)), "\n";' . "\n";;
+        $expect .= $expectedCanonicalExtJson . "\n";
 
-        $code .= "\n" . sprintf('$json = %s;', var_export($json, true)) . "\n";
-        $code .= "\n// extJSON to Canonical extJSON\n";
-        $code .= 'echo json_canonicalize(toExtendedJSON(fromJSON($json))), "\n";' . "\n";;
-        $expect .= $expectedCanonicalJson . "\n";
-
-        if ($bson !== $canonicalBson) {
-            $code .= "\n// Canonical BSON to Canonical extJSON\n";
-            $code .= 'echo json_canonicalize(toExtendedJSON($canonicalBson)), "\n";' . "\n";;
-            $expect .= $expectedCanonicalJson . "\n";
+        if (isset($relaxedExtJson)) {
+            $code .= "\n// Degenerate BSON -> Relaxed extJSON \n";
+            $code .= 'echo json_canonicalize(toJSON($degenerateBson)), "\n";' . "\n";;
+            $expect .= $expectedRelaxedExtJson . "\n";
         }
+    }
 
-        if ($json !== $canonicalJson) {
-            $code .= "\n" . sprintf('$canonicalJson = %s;', var_export($canonicalJson, true)) . "\n";
-            $code .= "\n// Canonical extJSON to Canonical extJSON\n";
-            $code .= 'echo json_canonicalize(toExtendedJSON(fromJSON($canonicalJson))), "\n";' . "\n";;
-            $expect .= $expectedCanonicalJson . "\n";
-        }
+    if (isset($degenerateExtJson) && !$lossy) {
+        $code .= "\n// Degenerate extJSON -> Canonical BSON \n";
+        $code .= 'echo bin2hex(fromJSON($degenerateExtJson)), "\n";' . "\n";
+        $expect .= $expectedCanonicalBson . "\n";
+    }
 
-        if ( ! $lossy) {
-            $code .= "\n// extJSON to Canonical BSON\n";
-            $code .= 'echo bin2hex(fromJSON($json)), "\n";' . "\n";;
-            $expect .= $expectedCanonicalBson . "\n";
-
-            if ($json !== $canonicalJson) {
-                $code .= "\n// Canonical extJSON to Canonical BSON\n";
-                $code .= 'echo bin2hex(fromJSON($canonicalJson)), "\n";' . "\n";;
-                $expect .= $expectedCanonicalBson . "\n";
-            }
-        }
+    if (isset($relaxedExtJson)) {
+        $code .= "\n// Relaxed extJSON -> BSON -> Relaxed extJSON \n";
+        $code .= 'echo json_canonicalize(toJSON(fromJSON($relaxedExtJson))), "\n";' . "\n";
+        $expect .= $expectedRelaxedExtJson . "\n";
     }
 
     return [
@@ -140,6 +191,12 @@ function getParamsForValid(array $test, array $case)
 
 function getParamsForDecodeError(array $test, array $case)
 {
+    foreach (['description', 'bson'] as $field) {
+        if (!isset($case[$field])) {
+            throw new InvalidArgumentException(sprintf('Missing "%s" field', $field));
+        }
+    }
+
     $code = sprintf('$bson = hex2bin(%s);', var_export($case['bson'], true)) . "\n\n";
     $code .= "throws(function() use (\$bson) {\n";
     $code .= "    var_dump(toPHP(\$bson));\n";
@@ -158,10 +215,26 @@ function getParamsForDecodeError(array $test, array $case)
 
 function getParamsForParseError(array $test, array $case)
 {
+    foreach (['description', 'string'] as $field) {
+        if (!isset($case[$field])) {
+            throw new InvalidArgumentException(sprintf('Missing "%s" field', $field));
+        }
+    }
+
     $code = '';
     $expect = '';
 
     switch ($test['bson_type']) {
+        case '0x00': // Top-level document
+            $code = "throws(function() {\n";
+            $code .= sprintf("    fromJSON(%s);\n", var_export($case['string'], true));
+            $code .= "}, 'MongoDB\Driver\Exception\UnexpectedValueException');";
+
+            /* We do not test for the exception message, since that may differ
+             * based on the nature of the parse error. */
+            $expect = "OK: Got MongoDB\Driver\Exception\UnexpectedValueException";
+            break;
+
         case '0x13': // Decimal128
             $code = "throws(function() {\n";
             $code .= sprintf("    new MongoDB\BSON\Decimal128(%s);\n", var_export($case['string'], true));
@@ -173,7 +246,7 @@ function getParamsForParseError(array $test, array $case)
             break;
 
         default:
-            throw new UnexpectedValueException;
+            throw new UnexpectedValueException(sprintf("Parse errors not supported for BSON type: %s", $test['bson_type']));
     }
 
     return [
