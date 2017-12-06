324d969fafe277a1cb25118aba94320593662db5
Jeremy Mikola <jmikola@gmail.com>
2017-12-06 17:47:24 -0500

    Bump 3.6.0-rc3 to 3.6.0 for Mongo Orchestration

--- scripts/ubuntu/mongo-orchestration.sh
+++ scripts/ubuntu/mongo-orchestration.sh
@@ -7,7 +7,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 58712A2291FA4AD5
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.2.list
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.0 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.0.list
 echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/2.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-2.6.list
-echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/testing multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-testing.list
+echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.6.list
 apt-get update
 
 apt-get install -y libsnmp15 libgsasl7 libcurl4-openssl-dev
@@ -16,12 +16,12 @@ sudo apt-get download mongodb-enterprise-server=3.2.0
 sudo apt-get download mongodb-enterprise-mongos=3.2.0
 sudo apt-get download mongodb-enterprise-server=3.0.3
 sudo apt-get download mongodb-enterprise-server=2.6.9
-sudo apt-get download mongodb-enterprise-server=3.6.0~rc3
+sudo apt-get download mongodb-enterprise-server=3.6.0
 dpkg -x mongodb-enterprise-server_2.6.9_amd64.deb 2.6.9
 dpkg -x mongodb-enterprise-server_3.0.3_amd64.deb 3.0.3
 dpkg -x mongodb-enterprise-server_3.2.0_amd64.deb 3.2.0
 dpkg -x mongodb-enterprise-mongos_3.2.0_amd64.deb 3.2.0
-dpkg -x mongodb-enterprise-server_3.6.0~rc3_amd64.deb 3.6.0rc3
+dpkg -x mongodb-enterprise-server_3.6.0_amd64.deb 3.6.0
 
 
 # Python stuff for mongo-orchestration
