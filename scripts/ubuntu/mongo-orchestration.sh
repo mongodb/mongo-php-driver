9812d6713ef030259fef06443bd6002083b71987
Jeremy Mikola <jmikola@gmail.com>
2018-01-06 21:18:01 -0500

    Upgrade VM to trusty64 and revise mo installation
    This also removes unused server versions.

--- scripts/ubuntu/mongo-orchestration.sh
+++ scripts/ubuntu/mongo-orchestration.sh
@@ -1,34 +1,28 @@
-# Enable MongoDB Enterprise repo
+# 3.0
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-# 3.2 key
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-# testing key
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 58712A2291FA4AD5
-echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.2.list
-echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.0 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.0.list
-echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/2.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-2.6.list
-echo 'deb http://repo.mongodb.com/apt/ubuntu precise/mongodb-enterprise/3.6 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise-3.6.list
+
+# 3.6
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+
+echo 'deb http://repo.mongodb.com/apt/ubuntu trusty/mongodb-enterprise/3.0 multiverse' | tee /etc/apt/sources.list.d/mongodb-enterprise-3.0.list
+echo 'deb http://repo.mongodb.com/apt/ubuntu trusty/mongodb-enterprise/3.6 multiverse' | tee /etc/apt/sources.list.d/mongodb-enterprise-3.6.list
+
 apt-get update
 
-apt-get install -y libsnmp15 libgsasl7 libcurl4-openssl-dev
+apt-get install -y libsnmp30 libgsasl7 libcurl4-openssl-dev
 
-sudo apt-get download mongodb-enterprise-server=3.2.0
-sudo apt-get download mongodb-enterprise-mongos=3.2.0
-sudo apt-get download mongodb-enterprise-server=3.0.3
-sudo apt-get download mongodb-enterprise-server=2.6.9
-sudo apt-get download mongodb-enterprise-server=3.6.0
-dpkg -x mongodb-enterprise-server_2.6.9_amd64.deb 2.6.9
-dpkg -x mongodb-enterprise-server_3.0.3_amd64.deb 3.0.3
-dpkg -x mongodb-enterprise-server_3.2.0_amd64.deb 3.2.0
-dpkg -x mongodb-enterprise-mongos_3.2.0_amd64.deb 3.2.0
-dpkg -x mongodb-enterprise-server_3.6.0_amd64.deb 3.6.0
-
+apt-get download mongodb-enterprise-server=3.0.15
+apt-get download mongodb-enterprise-server=3.6.1
+apt-get download mongodb-enterprise-mongos=3.6.1
+dpkg -x mongodb-enterprise-server_3.0.15_amd64.deb 3.0
+dpkg -x mongodb-enterprise-server_3.6.1_amd64.deb 3.6
+dpkg -x mongodb-enterprise-mongos_3.6.1_amd64.deb 3.6
 
 # Python stuff for mongo-orchestration
-apt-get install -y python python-dev python-pip
+apt-get install -y python python-dev
+python get-pip.py
 
-pip install --index-url=https://pypi.python.org/simple/ --upgrade 'git+https://github.com/10gen/mongo-orchestration.git#egg=mongo_orchestration'
+pip install --upgrade mongo-orchestration
 
 # Launch mongo-orchestration
 mongo-orchestration -f mongo-orchestration-config.json -b 192.168.112.10 --enable-majority-read-concern start
-
