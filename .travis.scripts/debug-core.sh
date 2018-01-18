df9c278d3715370ff1bb20b3827763f2bc742b30
Jeremy Mikola <jmikola@gmail.com>
2018-01-18 16:36:38 -0500

    Make core and diff debugging more portable

--- .travis.scripts/debug-core.sh
+++ .travis.scripts/debug-core.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-PHP=/home/travis/.phpenv/versions/`php -r 'echo PHP_VERSION;'`/bin/php
-echo "backtrace full" | gdb $PHP $1
+if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
+    # https://www.ics.uci.edu/~pattis/common/handouts/macmingweclipse/allexperimental/mac-gdb-install.html
+    echo "Cannot debug core files on macOS: ${1}"
+    exit 1
+fi
 
+PHP_BINARY=`which php`
+gdb -batch -ex "bt full" -ex "quit" "${PHP_BINARY}" "${1}"
