071f90baccad246e25647afb3f92a1c45f46a1a6
Jeremy Mikola <jmikola@gmail.com>
2018-01-19 13:25:12 -0500

    Upgrading run-tests.php is no longer necessary
    PHP 5.4 was the last version where run-tests.php failed to report a non-zero exit code after a failure. Since the extension requires 5.5+, we can rely on any existing run-tests.php file.

--- .travis.scripts/before_script.sh
+++ .travis.scripts/before_script.sh
@@ -11,9 +11,6 @@ phpize
 make all -j4
 make install
 
-# Use latest run-tests.php, as older versions don't report a non-zero exit code for failures
-wget -O run-tests.php https://raw.githubusercontent.com/php/php-src/master/run-tests.php
-
 # Add the extension to php.ini
 echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
