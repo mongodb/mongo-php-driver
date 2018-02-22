125c99fb6a0dd7abd340c1c7207252db68cefcff
Derick Rethans <github@derickrethans.nl>
2018-02-22 16:50:09 +0000

    PHPC-1118: Add detection of the correct clang-format binary

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -12,6 +12,29 @@ if test x"$1" = xchanged; then
 	FILES="$FILES1 $FILES2 $FILES3"
 fi
 
+# Find clang-format, we prefer -6.0, but also allow binaries without -suffix as
+# long as they're 6.0.0
+CLANG_FORMAT=
+
+which clang-format-6.0
+if [ $? = 0 ]; then
+	CLANG_FORMAT=`which clang-format-6.0`
+fi
+
+which clang-format
+if [ $? = 0 ]; then
+	VERSION=`clang-format -version | cut -d " " -f 3 | cut -c 1-3`
+	if [ x"$VERSION" = "x6.0" ]; then
+		CLANG_FORMAT=`which clang-format`
+	fi
+fi
+
+if [ x"$CLANG_FORMAT" = "x" ]; then
+	echo "Couldn't find the right clang-format (needs version 6.0)"
+	exit
+fi
+
+# Run formatter
 for i in $FILES; do
-	clang-format-7 -i $i
+	$CLANG_FORMAT -i $i
 done
