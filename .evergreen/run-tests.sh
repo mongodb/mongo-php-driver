30b1e80415842d980a83d619b1845f384e26ee2b
Katherine Walker <katherine.walker@mongodb.com>
2018-05-01 14:28:07 -0400

    Add config.yml

---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       AUTH                    Set to enable authentication. Defaults to
+#       "noauth"
+#       SSL                     Set to enable SSL. Defaults to "nossl"
+#       MONGODB_URI             Set the suggested connection MONGODB_URI
+#       (including credentials and topology info)
+#       MARCH                   Machine Architecture. Defaults to lowercase
+#       uname -m
+
+
+AUTH=${AUTH:-noauth}
+SSL=${SSL:-nossl}
+MONGODB_URI=${MONGODB_URI:-}
+
+if [ "$AUTH" != "noauth" ]; then
+  export MONGOC_TEST_USER="bob"
+  export MONGOC_TEST_PASSWORD="pwd123"
+fi
+
+if [ "$SSL" != "nossl" ]; then
+   export
+MONGOC_TEST_SSL_PEM_FILE="$DRIVERS_TOOLS/.evergreen/x509gen/client.pem"
+   export MONGOC_TEST_SSL_CA_FILE="$DRIVERS_TOOLS/.evergreen/x509gen/ca.pem"
+fi
+
+echo "Running $AUTH tests over $SSL, connecting to $MONGODB_URI"
+
+# Run the tests, and store the results in a Evergreen compatible JSON results
+# file
+make test
+
