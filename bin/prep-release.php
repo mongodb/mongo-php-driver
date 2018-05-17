741ada8532fa6aba1066d07002b1aa6e757971fa
Jeremy Mikola <jmikola@gmail.com>
2018-05-17 16:45:02 -0400

    Improve precision of file globs for bundled zlib

--- bin/prep-release.php
+++ bin/prep-release.php
@@ -51,7 +51,7 @@ function get_files() {
         "src/libmongoc/src/libbson/src/jsonsl/*.{c,h}",
         "src/libmongoc/src/libbson/VERSION*",
         "src/libmongoc/src/mongoc/*.{c,def,defs,h,h.in}",
-        "src/libmongoc/src/zlib-1.2.11/*.{c,def,defs,h,in}",
+        "src/libmongoc/src/zlib-1.2.11/*.{c,h,h.in}",
         "src/libmongoc/VERSION*",
       ),
       'test' => array(
