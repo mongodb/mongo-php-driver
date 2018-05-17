mongodb/mongo-php-driver

commit 197f3f7a8a73391cbe489bc900b9f1f1d47d3341

PHPC-1166: Update zend array recursion protection for PHP 7.3

phongo_compat.h
@@ -178,7 +178,9 @@
 #error Unsupported architecture (integers are neither 32-bit nor 64-bit)
 #endif
 
-void phongo_add_exception_prop(const char* prop, int prop_len, zval* value TSRMLS_DC);
+void      phongo_add_exception_prop(const char* prop, int prop_len, zval* value TSRMLS_DC);
+zend_bool php_phongo_zend_hash_apply_protection_begin(HashTable* ht);
+zend_bool php_phongo_zend_hash_apply_protection_end(HashTable* ht);
 
 #endif /* PHONGO_COMPAT_H */
 

phongo_compat.c
@@ -32,6 +32,58 @@ void phongo_add_exception_prop(const char* prop, int prop_len, zval* value TSRML
 	}
 }
 
+#ifdef ZEND_HASH_GET_APPLY_COUNT /* PHP 7.2 or earlier recursion protection */
+inline zend_bool php_phongo_zend_hash_apply_protection_begin(HashTable* ht)
+{
+	if (!ht) {
+		return 1;
+	}
+	if (ZEND_HASH_GET_APPLY_COUNT(ht) > 0) {
+		return 0;
+	}
+	if (ZEND_HASH_APPLY_PROTECTION(ht)) {
+		ZEND_HASH_INC_APPLY_COUNT(ht);
+	}
+	return 1;
+}
+
+inline zend_bool php_phongo_zend_hash_apply_protection_end(HashTable* ht)
+{
+	if (!ht) {
+		return 1;
+	}
+	if (ZEND_HASH_GET_APPLY_COUNT(ht) == 0) {
+		return 0;
+	}
+	if (ZEND_HASH_APPLY_PROTECTION(ht)) {
+		ZEND_HASH_DEC_APPLY_COUNT(ht);
+	}
+	return 1;
+}
+#else /* PHP 7.3 or later */
+inline zend_bool php_phongo_zend_hash_apply_protection_begin(zend_array* ht)
+{
+	if (GC_IS_RECURSIVE(ht)) {
+		return 0;
+	}
+	if (!(GC_FLAGS(ht) & GC_IMMUTABLE)) {
+		GC_PROTECT_RECURSION(ht);
+	}
+	return 1;
+}
+
+inline zend_bool php_phongo_zend_hash_apply_protection_end(zend_array* ht)
+{
+	if (!GC_IS_RECURSIVE(ht)) {
+		return 0;
+	}
+	if (!(GC_FLAGS(ht) & GC_IMMUTABLE)) {
+		GC_UNPROTECT_RECURSION(ht);
+	}
+	return 1;
+}
+#endif
+
 /*
  * Local variables:
  * tab-width: 4

src/bson-encode.c
@@ -353,15 +353,11 @@ try_again:
 				bson_t     child;
 				HashTable* tmp_ht = HASH_OF(entry);
 
-				if (tmp_ht && ZEND_HASH_GET_APPLY_COUNT(tmp_ht) > 0) {
+				if (!php_phongo_zend_hash_apply_protection_begin(tmp_ht)) {
 					char* path_string = php_phongo_field_path_as_string(field_path);
 					phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE TSRMLS_CC, "Detected recursion for field path \"%s\"", path_string);
 					efree(path_string);
 					break;
-				}
-
-				if (tmp_ht && ZEND_HASH_APPLY_PROTECTION(tmp_ht)) {
-					ZEND_HASH_INC_APPLY_COUNT(tmp_ht);
 				}
 
 				bson_append_array_begin(bson, key, key_len, &child);
@@ -371,9 +367,7 @@ try_again:
 				field_path->size--;
 				bson_append_array_end(bson, &child);
 
-				if (tmp_ht && ZEND_HASH_APPLY_PROTECTION(tmp_ht)) {
-					ZEND_HASH_DEC_APPLY_COUNT(tmp_ht);
-				}
+				php_phongo_zend_hash_apply_protection_end(tmp_ht);
 				break;
 			}
 			PHONGO_BREAK_INTENTIONALLY_MISSING
@@ -381,15 +375,11 @@ try_again:
 		case IS_OBJECT: {
 			HashTable* tmp_ht = HASH_OF(entry);
 
-			if (tmp_ht && ZEND_HASH_GET_APPLY_COUNT(tmp_ht) > 0) {
+			if (!php_phongo_zend_hash_apply_protection_begin(tmp_ht)) {
 				char* path_string = php_phongo_field_path_as_string(field_path);
 				phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE TSRMLS_CC, "Detected recursion for field path \"%s\"", path_string);
 				efree(path_string);
 				break;
-			}
-
-			if (tmp_ht && ZEND_HASH_APPLY_PROTECTION(tmp_ht)) {
-				ZEND_HASH_INC_APPLY_COUNT(tmp_ht);
 			}
 
 			php_phongo_field_path_write_type_at_current_level(field_path, PHONGO_FIELD_PATH_ITEM_DOCUMENT);
@@ -397,9 +387,7 @@ try_again:
 			php_phongo_bson_append_object(bson, field_path, flags, key, key_len, entry TSRMLS_CC);
 			field_path->size--;
 
-			if (tmp_ht && ZEND_HASH_APPLY_PROTECTION(tmp_ht)) {
-				ZEND_HASH_DEC_APPLY_COUNT(tmp_ht);
-			}
+			php_phongo_zend_hash_apply_protection_end(tmp_ht);
 			break;
 		}
 

commit 0ecc5df3091f0b20a8f7afa10f9b99cd986569ed

PHPC-1166: Don't assume true is defined in phongo_compat.h
This fixes a build failure introduced by 197f3f7a8a73391cbe489bc900b9f1f1d47d3341 in #835.

phongo_compat.h
@@ -138,7 +138,7 @@
 	} while (0)
 #define phongo_free_object_arg void
 #define phongo_zpp_char_len int
-#define ZEND_HASH_APPLY_PROTECTION(ht) true
+#define ZEND_HASH_APPLY_PROTECTION(ht) 1
 #define ZEND_HASH_GET_APPLY_COUNT(ht) ((ht)->nApplyCount)
 #define ZEND_HASH_DEC_APPLY_COUNT(ht) ((ht)->nApplyCount -= 1)
 #define ZEND_HASH_INC_APPLY_COUNT(ht) ((ht)->nApplyCount += 1)