9e9af8d1078be82299cecdbb04940e741f296f3d
Derick Rethans <github@derickrethans.nl>
2018-12-03 13:46:34 +0000

    Back to -dev

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.6.0alpha1"
-#define PHP_MONGODB_STABILITY "alpha"
-#define PHP_MONGODB_VERSION_DESC 1,6,0,1
+#define PHP_MONGODB_VERSION "1.6.0alpha2-dev"
+#define PHP_MONGODB_STABILITY "devel"
+#define PHP_MONGODB_VERSION_DESC 1,6,0,2
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
