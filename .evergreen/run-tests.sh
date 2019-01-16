04151e0212c3333bb4ad3364b284fdce3a351974
Derick Rethans <github@derickrethans.nl>
2019-01-16 16:12:20 +0000

    Remove superfluous files, and remove unused env vars from run-tests.sh

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -3,10 +3,8 @@ set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # Supported/used environment variables:
-#       AUTH                    Set to enable authentication. Defaults to "noauth"
 #       SSL                     Set to enable SSL. Defaults to "nossl"
 #       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
-#       MARCH                   Machine Architecture. Defaults to lowercase uname -m
 
 
 AUTH=${AUTH:-noauth}
