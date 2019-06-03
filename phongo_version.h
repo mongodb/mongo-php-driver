66d667dbd544da8e4b355ef7c26c61cb1feb265c
Jeremy Mikola <jmikola@gmail.com>
2019-06-03 13:54:07 -0400

    Back to -dev

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.5.4"
-#define PHP_MONGODB_STABILITY "stable"
-#define PHP_MONGODB_VERSION_DESC 1,5,4,1
+#define PHP_MONGODB_VERSION "1.5.5-dev"
+#define PHP_MONGODB_STABILITY "devel"
+#define PHP_MONGODB_VERSION_DESC 1,5,5,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
