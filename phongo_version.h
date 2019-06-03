f164a5071431dc8d66b3e0b5321dd05d2ed74cb4
Jeremy Mikola <jmikola@gmail.com>
2019-06-03 15:39:23 -0400

    Package 1.6.0alpha2

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.6.0alpha2-dev"
-#define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,6,0,2
+#define PHP_MONGODB_VERSION "1.6.0alpha2"
+#define PHP_MONGODB_STABILITY "alpha"
+#define PHP_MONGODB_VERSION_DESC 1,6,0,3
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
