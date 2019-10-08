79afd5dbea0aacae26d1ba299282b8275a0cb0c3
Andreas Braun <git@alcaeus.org>
2019-10-08 09:31:13 +0200

    PHPC-1230: Allow cloning ObjectID objects

--- src/BSON/ObjectId.c
+++ src/BSON/ObjectId.c
@@ -336,6 +336,34 @@ static phongo_create_object_retval php_phongo_objectid_create_object(zend_class_
 #endif
 } /* }}} */
 
+static phongo_create_object_retval php_phongo_objectid_clone_object(zval* object TSRMLS_DC) /* {{{ */
+{
+	php_phongo_objectid_t* intern;
+	php_phongo_objectid_t* new_intern;
+	phongo_create_object_retval new_object;
+
+	intern = Z_OBJECTID_OBJ_P(object);
+	new_object = php_phongo_objectid_create_object(Z_OBJCE_P(object) TSRMLS_CC);
+
+#if PHP_VERSION_ID >= 70000
+	new_intern = Z_OBJ_OBJECTID(new_object);
+	zend_objects_clone_members(&new_intern->std, &intern->std TSRMLS_CC);
+#else
+	{
+		zend_object_handle handle = Z_OBJ_HANDLE_P(object);
+
+		new_intern = (php_phongo_objectid_t*) zend_object_store_get_object_by_handle(new_object.handle TSRMLS_CC);
+		zend_objects_clone_members(&new_intern->std, new_object, &intern->std, handle TSRMLS_CC);
+	}
+#endif
+
+	// Use memcpy to copy bson value to avoid converting to string and back
+	memcpy(&new_intern->oid, &intern->oid, 25);
+	new_intern->initialized = true;
+
+	return new_object;
+} /* }}} */
+
 static int php_phongo_objectid_compare_objects(zval* o1, zval* o2 TSRMLS_DC) /* {{{ */
 {
 	php_phongo_objectid_t* intern1;
@@ -415,6 +443,7 @@ void php_phongo_objectid_init_ce(INIT_FUNC_ARGS) /* {{{ */
 	zend_class_implements(php_phongo_objectid_ce TSRMLS_CC, 1, zend_ce_serializable);
 
 	memcpy(&php_phongo_handler_objectid, phongo_get_std_object_handlers(), sizeof(zend_object_handlers));
+	php_phongo_handler_objectid.clone_obj       = php_phongo_objectid_clone_object;
 	php_phongo_handler_objectid.compare_objects = php_phongo_objectid_compare_objects;
 	php_phongo_handler_objectid.get_debug_info  = php_phongo_objectid_get_debug_info;
 	php_phongo_handler_objectid.get_gc          = php_phongo_objectid_get_gc;
