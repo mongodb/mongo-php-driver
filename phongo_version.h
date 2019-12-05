efeb22d615674d0834b0a521340e9a337c051a0b
Andreas Braun <git@alcaeus.org>
2019-12-05 19:46:19 +0100

    Package 1.6.1

--- phongo_version.h
+++ phongo_version.h
@@ -18,8 +18,8 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.6.1-dev"
-#define PHP_MONGODB_STABILITY "devel"
+#define PHP_MONGODB_VERSION "1.6.1"
+#define PHP_MONGODB_STABILITY "stable"
 #define PHP_MONGODB_VERSION_DESC 1,6,1,0
 /* clang-format on */
 
