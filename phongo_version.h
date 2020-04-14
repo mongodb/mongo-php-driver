0691fd56a1f71d74e14ca109a7965a425f220794
Andreas Braun <git@alcaeus.org>
2020-04-14 09:53:59 +0200

    Master branch is now 1.9-dev

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.8.0-dev"
+#define PHP_MONGODB_VERSION "1.9.0-dev"
 #define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,8,0,0
+#define PHP_MONGODB_VERSION_DESC 1,9,0,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
