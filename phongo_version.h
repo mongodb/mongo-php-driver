3abafa28dddd6a24f4b9c601ec90a4abd5a4fcfc
Jeremy Mikola <jmikola@gmail.com>
2020-07-16 20:03:18 -0400

    Package 1.8.0RC1

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.8.0beta3-dev"
-#define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,8,0,4
+#define PHP_MONGODB_VERSION "1.8.0RC1"
+#define PHP_MONGODB_STABILITY "beta"
+#define PHP_MONGODB_VERSION_DESC 1,8,0,5
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
