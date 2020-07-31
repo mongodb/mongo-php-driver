0962b420eaf5be286b7ed1ee5930b700aba80651
Andreas Braun <git@alcaeus.org>
2020-07-31 07:53:05 +0200

    Back to -dev

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.8.0"
-#define PHP_MONGODB_STABILITY "stable"
-#define PHP_MONGODB_VERSION_DESC 1,8,0,7
+#define PHP_MONGODB_VERSION "1.8.1-dev"
+#define PHP_MONGODB_STABILITY "devel"
+#define PHP_MONGODB_VERSION_DESC 1,8,1,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
