cff68274736bce09e89b82c2ae086568464f755e
Alexander Golin <agolin95@gmail.com>
2021-04-16 17:46:28 -0400

    clean up some last strays

--- src/MongoDB/ClientEncryption.c
+++ src/MongoDB/ClientEncryption.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 MongoDB, Inc.
+ * Copyright 2019-Present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/MongoDB/Exception/EncryptionException.c
+++ src/MongoDB/Exception/EncryptionException.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2020 MongoDB, Inc.
+ * Copyright 2014-Present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
