f743459d140e0ba4581b1dff2fb860716aca9004
Alexander Golin <agolin95@gmail.com>
2021-04-28 23:13:19 -0400

    change Present to present

--- src/MongoDB/Monitoring/functions.h
+++ src/MongoDB/Monitoring/functions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-Present MongoDB, Inc.
+ * Copyright 2016-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
