mongodb/mongo-php-driver

commit 324182a8e6c840297dad02a9bb6a37919f0d2b53

PHPC-1804: Manager::addSubscriber and removeSubscriber (#1213)
* Use zend_exception_ce instead of zend_exception_get_default()

zend_exception_get_default() has been deprecated since PHP 7.0. See: php/php-src@f9e9d3a43792846a483092bce409bc9927d24848

* Check retval from php_phongo_set_monitoring_callbacks

This ensures that php_phongo_client_register is never called in the event this method fails (however unlikely).

* Index subscriber HashTable by numeric object handles

This also updates add/removeSubscriber to use the new argument parsing macros from deead9687e4c294f74af0bc66f0813dcc54e3285.

* PHPC-1804: Manager::addSubscriber and removeSubscriber

Implements per-client event subscribers. Moves existing APM code to a new phongo_apm.c module and adds logic to coordinate dispatching between global and per-client subscribers.

src/phongo_apm.c
@@ -1,0 +1,353 @@
+/*
+ * Copyright 2021-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "bson/bson.h"
+#include "mongoc/mongoc.h"
+
+#include <php.h>
+#include <Zend/zend_exceptions.h>
+#include <Zend/zend_interfaces.h>
+#include <Zend/zend_operators.h>
+
+#include "phongo_compat.h"
+#include "php_phongo.h"
+#include "phongo_apm.h"
+
+#undef MONGOC_LOG_DOMAIN
+#define MONGOC_LOG_DOMAIN "PHONGO"
+
+ZEND_EXTERN_MODULE_GLOBALS(mongodb)
+
+/* Ensures that instances of @subscriber_ce in @from (those registered with a
+ * Manager or globally) are added to the set @to. This is used to build the list
+ * of subscribers notify for an event. */
+static void phongo_apm_add_subscribers_to_notify(zend_class_entry* subscriber_ce, HashTable* from, HashTable* to)
+{
+	zval* subscriber;
+
+	ZEND_HASH_FOREACH_VAL_IND(from, subscriber)
+	{
+		if (Z_TYPE_P(subscriber) != IS_OBJECT || !instanceof_function(Z_OBJCE_P(subscriber), subscriber_ce)) {
+			continue;
+		}
+
+		if (zend_hash_index_exists(to, Z_OBJ_HANDLE_P(subscriber))) {
+			continue;
+		}
+
+		zend_hash_index_update(to, Z_OBJ_HANDLE_P(subscriber), subscriber);
+		Z_ADDREF_P(subscriber);
+	}
+	ZEND_HASH_FOREACH_END();
+}
+
+/* Returns a newly allocated HashTable, which will contain all subscribers of a
+ * certain type that should be notified for an event on the specified client. */
+static HashTable* phongo_apm_get_subscribers_to_notify(zend_class_entry* subscriber_ce, mongoc_client_t* client)
+{
+	HashTable* subscribers = NULL;
+
+	ALLOC_HASHTABLE(subscribers);
+	zend_hash_init(subscribers, 0, NULL, ZVAL_PTR_DTOR, 0);
+
+	if (MONGODB_G(subscribers)) {
+		phongo_apm_add_subscribers_to_notify(subscriber_ce, MONGODB_G(subscribers), subscribers);
+	}
+
+	if (MONGODB_G(managers)) {
+		php_phongo_manager_t* manager;
+
+		ZEND_HASH_FOREACH_PTR(MONGODB_G(managers), manager)
+		{
+			if (manager->client == client && manager->subscribers) {
+				phongo_apm_add_subscribers_to_notify(subscriber_ce, manager->subscribers, subscribers);
+			}
+		}
+		ZEND_HASH_FOREACH_END();
+	}
+
+	return subscribers;
+}
+
+/* Search for a Manager associated with the given client in the request-scoped
+ * registry. If any Manager is found, copy it to @out, increment its ref-count,
+ * and return true; otherwise, set @out to undefined and return false. */
+static bool phongo_apm_copy_manager_for_client(mongoc_client_t* client, zval* out)
+{
+	php_phongo_manager_t* manager;
+
+	ZVAL_UNDEF(out);
+
+	if (!MONGODB_G(managers) || zend_hash_num_elements(MONGODB_G(managers)) == 0) {
+		return false;
+	}
+
+	ZEND_HASH_FOREACH_PTR(MONGODB_G(managers), manager)
+	{
+		if (manager->client == client) {
+			ZVAL_OBJ(out, &manager->std);
+			Z_ADDREF_P(out);
+
+			return true;
+		}
+	}
+	ZEND_HASH_FOREACH_END();
+
+	return false;
+}
+
+/* Dispatch an event to all subscribers in a HashTable. The caller is
+ * responsible for ensuring that subscribers implement the correct interface. */
+static void phongo_apm_dispatch_event(HashTable* subscribers, const char* function_name, zval* event)
+{
+	zval* subscriber;
+
+	ZEND_HASH_FOREACH_VAL_IND(subscribers, subscriber)
+	{
+		if (EG(exception)) {
+			break;
+		}
+
+		/* We can't use the zend_call_method_with_1_params macro here, as it
+		 * assumes the function name is a string literal. */
+		zend_call_method(PHONGO_COMPAT_OBJ_P(subscriber), NULL, NULL, function_name, strlen(function_name), NULL, 1, event, NULL);
+	}
+	ZEND_HASH_FOREACH_END();
+}
+
+static void phongo_apm_command_started(const mongoc_apm_command_started_t* event)
+{
+	mongoc_client_t*                  client;
+	HashTable*                        subscribers;
+	php_phongo_commandstartedevent_t* p_event;
+	zval                              z_event;
+
+	client      = mongoc_apm_command_started_get_context(event);
+	subscribers = phongo_apm_get_subscribers_to_notify(php_phongo_commandsubscriber_ce, client);
+
+	/* Return early if there are no APM subscribers to notify */
+	if (zend_hash_num_elements(subscribers) == 0) {
+		goto cleanup;
+	}
+
+	object_init_ex(&z_event, php_phongo_commandstartedevent_ce);
+	p_event = Z_COMMANDSTARTEDEVENT_OBJ_P(&z_event);
+
+	p_event->command_name  = estrdup(mongoc_apm_command_started_get_command_name(event));
+	p_event->server_id     = mongoc_apm_command_started_get_server_id(event);
+	p_event->operation_id  = mongoc_apm_command_started_get_operation_id(event);
+	p_event->request_id    = mongoc_apm_command_started_get_request_id(event);
+	p_event->command       = bson_copy(mongoc_apm_command_started_get_command(event));
+	p_event->database_name = estrdup(mongoc_apm_command_started_get_database_name(event));
+
+	if (!phongo_apm_copy_manager_for_client(client, &p_event->manager)) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
+		zval_ptr_dtor(&z_event);
+
+		goto cleanup;
+	}
+
+	phongo_apm_dispatch_event(subscribers, "commandStarted", &z_event);
+	zval_ptr_dtor(&z_event);
+
+cleanup:
+	zend_hash_destroy(subscribers);
+	FREE_HASHTABLE(subscribers);
+}
+
+static void phongo_apm_command_succeeded(const mongoc_apm_command_succeeded_t* event)
+{
+	mongoc_client_t*                    client;
+	HashTable*                          subscribers;
+	php_phongo_commandsucceededevent_t* p_event;
+	zval                                z_event;
+
+	client      = mongoc_apm_command_succeeded_get_context(event);
+	subscribers = phongo_apm_get_subscribers_to_notify(php_phongo_commandsubscriber_ce, client);
+
+	/* Return early if there are no APM subscribers to notify */
+	if (zend_hash_num_elements(subscribers) == 0) {
+		goto cleanup;
+	}
+
+	object_init_ex(&z_event, php_phongo_commandsucceededevent_ce);
+	p_event = Z_COMMANDSUCCEEDEDEVENT_OBJ_P(&z_event);
+
+	p_event->command_name    = estrdup(mongoc_apm_command_succeeded_get_command_name(event));
+	p_event->server_id       = mongoc_apm_command_succeeded_get_server_id(event);
+	p_event->operation_id    = mongoc_apm_command_succeeded_get_operation_id(event);
+	p_event->request_id      = mongoc_apm_command_succeeded_get_request_id(event);
+	p_event->duration_micros = mongoc_apm_command_succeeded_get_duration(event);
+	p_event->reply           = bson_copy(mongoc_apm_command_succeeded_get_reply(event));
+
+	if (!phongo_apm_copy_manager_for_client(client, &p_event->manager)) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
+		zval_ptr_dtor(&z_event);
+
+		goto cleanup;
+	}
+
+	phongo_apm_dispatch_event(subscribers, "commandSucceeded", &z_event);
+	zval_ptr_dtor(&z_event);
+
+cleanup:
+	zend_hash_destroy(subscribers);
+	FREE_HASHTABLE(subscribers);
+}
+
+static void phongo_apm_command_failed(const mongoc_apm_command_failed_t* event)
+{
+	mongoc_client_t*                 client;
+	HashTable*                       subscribers;
+	php_phongo_commandfailedevent_t* p_event;
+	zval                             z_event;
+	bson_error_t                     tmp_error = { 0 };
+
+	client      = mongoc_apm_command_failed_get_context(event);
+	subscribers = phongo_apm_get_subscribers_to_notify(php_phongo_commandsubscriber_ce, client);
+
+	/* Return early if there are no APM subscribers to notify */
+	if (zend_hash_num_elements(subscribers) == 0) {
+		goto cleanup;
+	}
+
+	object_init_ex(&z_event, php_phongo_commandfailedevent_ce);
+	p_event = Z_COMMANDFAILEDEVENT_OBJ_P(&z_event);
+
+	p_event->command_name    = estrdup(mongoc_apm_command_failed_get_command_name(event));
+	p_event->server_id       = mongoc_apm_command_failed_get_server_id(event);
+	p_event->operation_id    = mongoc_apm_command_failed_get_operation_id(event);
+	p_event->request_id      = mongoc_apm_command_failed_get_request_id(event);
+	p_event->duration_micros = mongoc_apm_command_failed_get_duration(event);
+	p_event->reply           = bson_copy(mongoc_apm_command_failed_get_reply(event));
+
+	if (!phongo_apm_copy_manager_for_client(client, &p_event->manager)) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
+		zval_ptr_dtor(&z_event);
+
+		goto cleanup;
+	}
+
+	/* We need to process and convert the error right here, otherwise
+	 * debug_info will turn into a recursive loop, and with the wrong trace
+	 * locations */
+	mongoc_apm_command_failed_get_error(event, &tmp_error);
+
+	object_init_ex(&p_event->z_error, phongo_exception_from_mongoc_domain(tmp_error.domain, tmp_error.code));
+	zend_update_property_string(zend_ce_exception, PHONGO_COMPAT_OBJ_P(&p_event->z_error), ZEND_STRL("message"), tmp_error.message);
+	zend_update_property_long(zend_ce_exception, PHONGO_COMPAT_OBJ_P(&p_event->z_error), ZEND_STRL("code"), tmp_error.code);
+
+	phongo_apm_dispatch_event(subscribers, "commandFailed", &z_event);
+	zval_ptr_dtor(&z_event);
+
+cleanup:
+	zend_hash_destroy(subscribers);
+	FREE_HASHTABLE(subscribers);
+}
+
+/* Assigns APM callbacks to a client, which will notify any global or per-client
+ * subscribers. This should be called for all clients created by the driver.
+ * Returns true on success; otherwise, throws an exception and returns false. */
+bool phongo_apm_set_callbacks(mongoc_client_t* client)
+{
+	bool retval;
+
+	mongoc_apm_callbacks_t* callbacks = mongoc_apm_callbacks_new();
+
+	mongoc_apm_set_command_started_cb(callbacks, phongo_apm_command_started);
+	mongoc_apm_set_command_succeeded_cb(callbacks, phongo_apm_command_succeeded);
+	mongoc_apm_set_command_failed_cb(callbacks, phongo_apm_command_failed);
+
+	retval = mongoc_client_set_apm_callbacks(client, callbacks, client);
+
+	if (!retval) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Failed to set APM callbacks");
+	}
+
+	mongoc_apm_callbacks_destroy(callbacks);
+
+	return retval;
+}
+
+/* Checks args for adding/removing a subscriber. Returns true on success;
+ * otherwise, throws an exception and returns false. */
+static bool phongo_apm_check_args_for_add_and_remove(HashTable* subscribers, zval* subscriber)
+{
+	if (!subscribers) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Subscriber HashTable is not initialized");
+		return false;
+	}
+
+	if (!subscriber || Z_TYPE_P(subscriber) != IS_OBJECT || !instanceof_function(Z_OBJCE_P(subscriber), php_phongo_subscriber_ce)) {
+		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Subscriber is not an instance of %s", ZSTR_VAL(php_phongo_subscriber_ce->name));
+		return false;
+	}
+
+	return true;
+}
+
+/* Adds a subscriber to the HashTable (global or Manager). Returns true on
+ * success (including NOP if already registered); otherwise, throws an exception
+ * and returns false. */
+bool phongo_apm_add_subscriber(HashTable* subscribers, zval* subscriber)
+{
+	if (!phongo_apm_check_args_for_add_and_remove(subscribers, subscriber)) {
+		/* Exception should already have been thrown */
+		return false;
+	}
+
+	/* NOP if the subscriber was already registered */
+	if (zend_hash_index_exists(subscribers, Z_OBJ_HANDLE_P(subscriber))) {
+		return true;
+	}
+
+	zend_hash_index_update(subscribers, Z_OBJ_HANDLE_P(subscriber), subscriber);
+	Z_ADDREF_P(subscriber);
+
+	return true;
+}
+
+/* Removes a subscriber from the HashTable (global or Manager). Returns true on
+ * success (including NOP if never registered); otherwise, throws an exception
+ * and returns false. */
+bool phongo_apm_remove_subscriber(HashTable* subscribers, zval* subscriber)
+{
+	if (!phongo_apm_check_args_for_add_and_remove(subscribers, subscriber)) {
+		/* Exception should already have been thrown */
+		return false;
+	}
+
+	/* Note: HashTables should specify ZVAL_PTR_DTOR as their element destructor
+	 * so there is no need to decrement the subscriber's reference count here.
+	 * We also don't care about whether zend_hash_index_del returns SUCCESS or
+	 * FAILURE, as removing an unregistered subscriber is a NOP. */
+	zend_hash_index_del(subscribers, Z_OBJ_HANDLE_P(subscriber));
+
+	return true;
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */

config.m4
@@ -105,6 +105,7 @@ if test "$PHP_MONGODB" != "no"; then
     phongo_compat.c \
     src/bson.c \
     src/bson-encode.c \
+    src/phongo_apm.c \
     src/BSON/Binary.c \
     src/BSON/BinaryInterface.c \
     src/BSON/DBPointer.c \

config.w32
@@ -118,7 +118,7 @@ if (PHP_MONGODB != "no") {
   var PHP_MONGODB_MONGOC_SOURCES="mongoc-aggregate.c mongoc-apm.c mongoc-array.c mongoc-async.c mongoc-async-cmd.c mongoc-buffer.c mongoc-bulk-operation.c mongoc-change-stream.c mongoc-client.c mongoc-client-pool.c mongoc-client-session.c mongoc-client-side-encryption.c mongoc-cluster-aws.c mongoc-cluster.c mongoc-cluster-cyrus.c mongoc-cluster-sasl.c mongoc-cluster-sspi.c mongoc-cmd.c mongoc-collection.c mongoc-compression.c mongoc-counters.c mongoc-crypt.c mongoc-crypto.c mongoc-crypto-cng.c mongoc-crypto-common-crypto.c mongoc-crypto-openssl.c mongoc-cursor-array.c mongoc-cursor.c mongoc-cursor-change-stream.c mongoc-cursor-cmd.c mongoc-cursor-cmd-deprecated.c mongoc-cursor-find.c mongoc-cursor-find-cmd.c mongoc-cursor-find-opquery.c mongoc-cursor-legacy.c mongoc-cyrus.c mongoc-database.c mongoc-error.c mongoc-find-and-modify.c mongoc-gridfs-bucket.c mongoc-gridfs-bucket-file.c mongoc-gridfs.c mongoc-gridfs-file.c mongoc-gridfs-file-list.c mongoc-gridfs-file-page.c mongoc-handshake.c mongoc-host-list.c mongoc-http.c mongoc-index.c mongoc-init.c mongoc-interrupt.c mongoc-libressl.c mongoc-linux-distro-scanner.c mongoc-list.c mongoc-log.c mongoc-matcher.c mongoc-matcher-op.c mongoc-memcmp.c mongoc-ocsp-cache.c mongoc-openssl.c mongoc-optional.c mongoc-opts.c mongoc-opts-helpers.c mongoc-queue.c mongoc-rand-cng.c mongoc-rand-common-crypto.c mongoc-rand-openssl.c mongoc-read-concern.c mongoc-read-prefs.c mongoc-rpc.c mongoc-sasl.c mongoc-scram.c mongoc-secure-channel.c mongoc-secure-transport.c mongoc-server-api.c mongoc-server-description.c mongoc-server-monitor.c mongoc-server-stream.c mongoc-set.c mongoc-socket.c mongoc-ssl.c mongoc-sspi.c mongoc-stream-buffered.c mongoc-stream.c mongoc-stream-file.c mongoc-stream-gridfs.c mongoc-stream-gridfs-download.c mongoc-stream-gridfs-upload.c mongoc-stream-socket.c mongoc-stream-tls.c mongoc-stream-tls-libressl.c mongoc-stream-tls-openssl-bio.c mongoc-stream-tls-openssl.c mongoc-stream-tls-secure-channel.c mongoc-stream-tls-secure-transport.c mongoc-timeout.c mongoc-topology-background-monitoring.c mongoc-topology.c mongoc-topology-description-apm.c mongoc-topology-description.c mongoc-topology-scanner.c mongoc-uri.c mongoc-util.c mongoc-version-functions.c mongoc-write-command.c mongoc-write-command-legacy.c mongoc-write-concern.c";
 
   EXTENSION("mongodb", "php_phongo.c phongo_compat.c", null, PHP_MONGODB_CFLAGS);
-  MONGODB_ADD_SOURCES("/src", "bson.c bson-encode.c");
+  MONGODB_ADD_SOURCES("/src", "bson.c bson-encode.c phongo_apm.c");
   MONGODB_ADD_SOURCES("/src/BSON", "Binary.c BinaryInterface.c DBPointer.c Decimal128.c Decimal128Interface.c Int64.c Javascript.c JavascriptInterface.c MaxKey.c MaxKeyInterface.c MinKey.c MinKeyInterface.c ObjectId.c ObjectIdInterface.c Persistable.c Regex.c RegexInterface.c Serializable.c Symbol.c Timestamp.c TimestampInterface.c Type.c Undefined.c Unserializable.c UTCDateTime.c UTCDateTimeInterface.c functions.c");
   MONGODB_ADD_SOURCES("/src/MongoDB", "BulkWrite.c ClientEncryption.c Command.c Cursor.c CursorId.c CursorInterface.c Manager.c Query.c ReadConcern.c ReadPreference.c Server.c ServerApi.c Session.c WriteConcern.c WriteConcernError.c WriteError.c WriteResult.c");
   MONGODB_ADD_SOURCES("/src/MongoDB/Exception", "AuthenticationException.c BulkWriteException.c CommandException.c ConnectionException.c ConnectionTimeoutException.c EncryptionException.c Exception.c ExecutionTimeoutException.c InvalidArgumentException.c LogicException.c RuntimeException.c ServerException.c SSLConnectionException.c UnexpectedValueException.c WriteException.c");

php_phongo.c
@@ -66,6 +66,7 @@
 /* Our stuffz */
 #include "php_phongo.h"
 #include "php_bson.h"
+#include "src/phongo_apm.h"
 #include "src/BSON/functions.h"
 #include "src/MongoDB/Monitoring/functions.h"
 
@@ -2330,181 +2331,6 @@ static bool php_phongo_apply_driver_options_to_uri(mongoc_uri_t* uri, zval* zopt
 }
 #endif
 
-/* APM callbacks */
-static void php_phongo_dispatch_handlers(const char* name, zval* z_event)
-{
-	zval* value;
-
-	ZEND_HASH_FOREACH_VAL_IND(MONGODB_G(subscribers), value)
-	{
-		if (EG(exception)) {
-			break;
-		}
-		/* We can't use the zend_call_method_with_1_params macro here, as it
-		 * does a sizeof() on the name argument, which does only work with
-		 * constant names, but not with parameterized ones as it does
-		 * "sizeof(char*)" in that case. */
-		zend_call_method(PHONGO_COMPAT_OBJ_P(value), NULL, NULL, name, strlen(name), NULL, 1, z_event, NULL);
-	}
-	ZEND_HASH_FOREACH_END();
-}
-
-/* Search for a Manager associated with the given client in the request-scoped
- * registry. If any Manager is found, copy it into the output parameter
- * (incrementing its ref-count) and return true; otherwise, set the output
- * parameter to undefined and return false. */
-static bool php_phongo_copy_manager_for_client(mongoc_client_t* client, zval* out)
-{
-	php_phongo_manager_t* manager;
-
-	if (!MONGODB_G(managers) || zend_hash_num_elements(MONGODB_G(managers)) == 0) {
-		return false;
-	}
-
-	ZEND_HASH_FOREACH_PTR(MONGODB_G(managers), manager)
-	{
-		if (manager->client == client) {
-			ZVAL_OBJ(out, &manager->std);
-			Z_ADDREF_P(out);
-
-			return true;
-		}
-	}
-	ZEND_HASH_FOREACH_END();
-
-	ZVAL_UNDEF(out);
-
-	return false;
-}
-
-static void php_phongo_command_started(const mongoc_apm_command_started_t* event)
-{
-	php_phongo_commandstartedevent_t* p_event;
-	zval                              z_event;
-
-	/* Return early if there are no APM subscribers to notify */
-	if (!MONGODB_G(subscribers) || zend_hash_num_elements(MONGODB_G(subscribers)) == 0) {
-		return;
-	}
-
-	object_init_ex(&z_event, php_phongo_commandstartedevent_ce);
-	p_event = Z_COMMANDSTARTEDEVENT_OBJ_P(&z_event);
-
-	p_event->command_name  = estrdup(mongoc_apm_command_started_get_command_name(event));
-	p_event->server_id     = mongoc_apm_command_started_get_server_id(event);
-	p_event->operation_id  = mongoc_apm_command_started_get_operation_id(event);
-	p_event->request_id    = mongoc_apm_command_started_get_request_id(event);
-	p_event->command       = bson_copy(mongoc_apm_command_started_get_command(event));
-	p_event->database_name = estrdup(mongoc_apm_command_started_get_database_name(event));
-
-	if (!php_phongo_copy_manager_for_client(mongoc_apm_command_started_get_context(event), &p_event->manager)) {
-		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
-		zval_ptr_dtor(&z_event);
-
-		return;
-	}
-
-	php_phongo_dispatch_handlers("commandStarted", &z_event);
-	zval_ptr_dtor(&z_event);
-}
-
-static void php_phongo_command_succeeded(const mongoc_apm_command_succeeded_t* event)
-{
-	php_phongo_commandsucceededevent_t* p_event;
-	zval                                z_event;
-
-	/* Return early if there are no APM subscribers to notify */
-	if (!MONGODB_G(subscribers) || zend_hash_num_elements(MONGODB_G(subscribers)) == 0) {
-		return;
-	}
-
-	object_init_ex(&z_event, php_phongo_commandsucceededevent_ce);
-	p_event = Z_COMMANDSUCCEEDEDEVENT_OBJ_P(&z_event);
-
-	p_event->command_name    = estrdup(mongoc_apm_command_succeeded_get_command_name(event));
-	p_event->server_id       = mongoc_apm_command_succeeded_get_server_id(event);
-	p_event->operation_id    = mongoc_apm_command_succeeded_get_operation_id(event);
-	p_event->request_id      = mongoc_apm_command_succeeded_get_request_id(event);
-	p_event->duration_micros = mongoc_apm_command_succeeded_get_duration(event);
-	p_event->reply           = bson_copy(mongoc_apm_command_succeeded_get_reply(event));
-
-	if (!php_phongo_copy_manager_for_client(mongoc_apm_command_succeeded_get_context(event), &p_event->manager)) {
-		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
-		zval_ptr_dtor(&z_event);
-
-		return;
-	}
-
-	php_phongo_dispatch_handlers("commandSucceeded", &z_event);
-	zval_ptr_dtor(&z_event);
-}
-
-static void php_phongo_command_failed(const mongoc_apm_command_failed_t* event)
-{
-	php_phongo_commandfailedevent_t* p_event;
-	zval                             z_event;
-	bson_error_t                     tmp_error = { 0 };
-	zend_class_entry*                default_exception_ce;
-
-	default_exception_ce = zend_exception_get_default();
-
-	/* Return early if there are no APM subscribers to notify */
-	if (!MONGODB_G(subscribers) || zend_hash_num_elements(MONGODB_G(subscribers)) == 0) {
-		return;
-	}
-
-	object_init_ex(&z_event, php_phongo_commandfailedevent_ce);
-	p_event = Z_COMMANDFAILEDEVENT_OBJ_P(&z_event);
-
-	p_event->command_name    = estrdup(mongoc_apm_command_failed_get_command_name(event));
-	p_event->server_id       = mongoc_apm_command_failed_get_server_id(event);
-	p_event->operation_id    = mongoc_apm_command_failed_get_operation_id(event);
-	p_event->request_id      = mongoc_apm_command_failed_get_request_id(event);
-	p_event->duration_micros = mongoc_apm_command_failed_get_duration(event);
-	p_event->reply           = bson_copy(mongoc_apm_command_failed_get_reply(event));
-
-	if (!php_phongo_copy_manager_for_client(mongoc_apm_command_failed_get_context(event), &p_event->manager)) {
-		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Found no Manager for client in APM event context");
-		zval_ptr_dtor(&z_event);
-
-		return;
-	}
-
-	/* We need to process and convert the error right here, otherwise
-	 * debug_info will turn into a recursive loop, and with the wrong trace
-	 * locations */
-	mongoc_apm_command_failed_get_error(event, &tmp_error);
-
-	object_init_ex(&p_event->z_error, phongo_exception_from_mongoc_domain(tmp_error.domain, tmp_error.code));
-	zend_update_property_string(default_exception_ce, PHONGO_COMPAT_OBJ_P(&p_event->z_error), ZEND_STRL("message"), tmp_error.message);
-	zend_update_property_long(default_exception_ce, PHONGO_COMPAT_OBJ_P(&p_event->z_error), ZEND_STRL("code"), tmp_error.code);
-
-	php_phongo_dispatch_handlers("commandFailed", &z_event);
-	zval_ptr_dtor(&z_event);
-}
-
-/* Sets the callbacks for APM */
-bool php_phongo_set_monitoring_callbacks(mongoc_client_t* client)
-{
-	bool retval;
-
-	mongoc_apm_callbacks_t* callbacks = mongoc_apm_callbacks_new();
-
-	mongoc_apm_set_command_started_cb(callbacks, php_phongo_command_started);
-	mongoc_apm_set_command_succeeded_cb(callbacks, php_phongo_command_succeeded);
-	mongoc_apm_set_command_failed_cb(callbacks, php_phongo_command_failed);
-
-	retval = mongoc_client_set_apm_callbacks(client, callbacks, client);
-
-	if (!retval) {
-		phongo_throw_exception(PHONGO_ERROR_UNEXPECTED_VALUE, "Failed to set APM callbacks");
-	}
-
-	mongoc_apm_callbacks_destroy(callbacks);
-
-	return retval;
-}
-
 static zval* php_phongo_manager_prepare_manager_for_hash(zval* driverOptions, bool* free)
 {
 	php_phongo_manager_t* manager;
@@ -3485,7 +3311,10 @@ void phongo_manager_init(php_phongo_manager_t* manager, const char* uri_string, 
 		goto cleanup;
 	}
 
-	php_phongo_set_monitoring_callbacks(manager->client);
+	if (!phongo_apm_set_callbacks(manager->client)) {
+		/* Exception should already have been thrown */
+		goto cleanup;
+	}
 
 	MONGOC_DEBUG("Created client with hash: %s", manager->client_hash);
 

php_phongo_structs.h
@@ -75,6 +75,7 @@ typedef struct {
 	size_t           client_hash_len;
 	bool             use_persistent_client;
 	zval             key_vault_client_manager;
+	HashTable*       subscribers;
 	zend_object      std;
 } php_phongo_manager_t;
 

commit 717fa6b2954896c9fb263272967b8eba83a20835

PHPC-1870 and PHPC-1872: Fix zSeries builds and testing with RS arbiters (#1232)
* PHPC-1870: Include config.h after php.h to avoid redef of WORDS_BIGENDIAN

This should fix zSeries build failures introduced by 324182a8e6c840297dad02a9bb6a37919f0d2b53

* PHPC-1872: Count data-bearing nodes for write concern

WC using the total server count cannot be satisfied if the RS includes arbiters.

src/phongo_apm.c
@@ -14,10 +14,6 @@
  * limitations under the License.
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include "bson/bson.h"
 #include "mongoc/mongoc.h"
 
@@ -25,6 +21,10 @@
 #include <Zend/zend_exceptions.h>
 #include <Zend/zend_interfaces.h>
 #include <Zend/zend_operators.h>
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include "phongo_compat.h"
 #include "php_phongo.h"

tests/server/server-executeQuery-008.phpt
@@ -13,11 +13,15 @@ $manager = create_test_manager();
 
 $primaryRp = new MongoDB\Driver\ReadPreference(MongoDB\Driver\ReadPreference::RP_PRIMARY);
 $primary = $manager->selectServer($primaryRp);
-$serverCount = count($manager->getServers());
+
+// Count all data-bearing members to use for the write concern
+$dataBearingNodes = count(array_filter($manager->getServers(), function (MongoDB\Driver\Server $server) {
+    return ($server->isPrimary() || $server->isSecondary());
+}));
 
 $bulk = new \MongoDB\Driver\BulkWrite;
 $bulk->insert(['_id' => 1, 'x' => 1]);
-$primary->executeBulkWrite(NS, $bulk, new MongoDB\Driver\WriteConcern($serverCount));
+$primary->executeBulkWrite(NS, $bulk, new MongoDB\Driver\WriteConcern($dataBearingNodes));
 
 $secondaryRp = new MongoDB\Driver\ReadPreference(MongoDB\Driver\ReadPreference::RP_SECONDARY);
 $secondary = $manager->selectServer($secondaryRp);