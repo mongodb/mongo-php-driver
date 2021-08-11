e5f4bf896bb23c413b54a6fa8dd61ce08a14de0a
Tanli Su <46271307+tanlisu@users.noreply.github.com>
2021-08-11 15:45:43 -0400

    PHPC-1934: Check for uninitialized intern in ServerDescription get_properties_hash

--- src/MongoDB/ServerDescription.c
+++ src/MongoDB/ServerDescription.c
@@ -177,6 +177,10 @@ HashTable* php_phongo_serverdescription_get_properties_hash(phongo_compat_object
 	intern = Z_OBJ_SERVERDESCRIPTION(PHONGO_COMPAT_GET_OBJ(object));
 
 	PHONGO_GET_PROPERTY_HASH_INIT_PROPS(is_debug, intern, props, 6);
+	
+	if (!intern->server_description) {
+		return props;
+	}
 
 	{
 		zval                host, port, type;
