25eb81dccf684275735f08d1d08799cd43db74d9
Jeremy Mikola <jmikola@gmail.com>
2021-11-02 14:57:09 -0400

    Master branch is now 1.12-dev

--- phongo_version.h
+++ phongo_version.h
@@ -18,9 +18,9 @@
 #define PHONGO_VERSION_H
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.11.1-dev"
+#define PHP_MONGODB_VERSION "1.12.0-dev"
 #define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,11,1,0
+#define PHP_MONGODB_VERSION_DESC 1,12,0,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
