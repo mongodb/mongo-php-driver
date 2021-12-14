mongodb/mongo-php-driver

commit 6c82ec8b52fad33c3e9444ae8bd2c27a729c4688

PHPC-2004 and PHPC-2007: Specify __toString() return type and implement Stringable (#1274)
* PHPC-2004: Specify string return type in __toString() arginfos

Adding return type info to the interface methods is a small subtle BC break. Userland classes will also need to add return type info prior to PHP 8.1, where it is added automatically. However, to leave the interface methods untyped would invite an error because BSON classes cannot otherwise conform to Stringable, which has return type info.

Additionally, PHP 8.2 will start raising a warning when return type info is automatically applied, so a BC break in some form is inevitable.

* PHPC-2007: Explicitly implement Stringable for PHP 8+

* Fix code formatting

This was missed in fe99deb32a73539cefd6e9a623ce15a439e8edba

src/BSON/BinaryInterface.c
@@ -26,17 +26,20 @@
 zend_class_entry* php_phongo_binary_interface_ce;
 
 /* {{{ MongoDB\BSON\BinaryInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_BinaryInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_BinaryInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_binary_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(BinaryInterface, getData, ai_BinaryInterface_void)
 	ZEND_ABSTRACT_ME(BinaryInterface, getType, ai_BinaryInterface_void)
-	ZEND_ABSTRACT_ME(BinaryInterface, __toString, ai_BinaryInterface_void)
+	ZEND_ABSTRACT_ME(BinaryInterface, __toString, ai_BinaryInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_binary_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/Decimal128Interface.c
@@ -26,15 +26,15 @@
 zend_class_entry* php_phongo_decimal128_interface_ce;
 
 /* {{{ MongoDB\BSON\Decimal128Interface function entries */
-ZEND_BEGIN_ARG_INFO_EX(ai_Decimal128Interface_void, 0, 0, 0)
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_Decimal128Interface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_decimal128_interface_me[] = {
-	/* clang-format off */
-	ZEND_ABSTRACT_ME(Decimal128Interface, __toString, ai_Decimal128Interface_void)
+	ZEND_ABSTRACT_ME(Decimal128Interface, __toString, ai_Decimal128Interface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_decimal128_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/JavascriptInterface.c
@@ -26,17 +26,20 @@
 zend_class_entry* php_phongo_javascript_interface_ce;
 
 /* {{{ MongoDB\BSON\JavascriptInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_JavascriptInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_JavascriptInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_javascript_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(JavascriptInterface, getCode, ai_JavascriptInterface_void)
 	ZEND_ABSTRACT_ME(JavascriptInterface, getScope, ai_JavascriptInterface_void)
-	ZEND_ABSTRACT_ME(JavascriptInterface, __toString, ai_JavascriptInterface_void)
+	ZEND_ABSTRACT_ME(JavascriptInterface, __toString, ai_JavascriptInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_javascript_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/ObjectIdInterface.c
@@ -26,16 +26,19 @@
 zend_class_entry* php_phongo_objectid_interface_ce;
 
 /* {{{ MongoDB\BSON\ObjectIdInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_ObjectIdInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_ObjectIdInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_objectid_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(ObjectIdInterface, getTimestamp, ai_ObjectIdInterface_void)
-	ZEND_ABSTRACT_ME(ObjectIdInterface, __toString, ai_ObjectIdInterface_void)
+	ZEND_ABSTRACT_ME(ObjectIdInterface, __toString, ai_ObjectIdInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_objectid_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/RegexInterface.c
@@ -26,17 +26,20 @@
 zend_class_entry* php_phongo_regex_interface_ce;
 
 /* {{{ MongoDB\BSON\RegexInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_RegexInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_RegexInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_regex_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(RegexInterface, getFlags, ai_RegexInterface_void)
 	ZEND_ABSTRACT_ME(RegexInterface, getPattern, ai_RegexInterface_void)
-	ZEND_ABSTRACT_ME(RegexInterface, __toString, ai_RegexInterface_void)
+	ZEND_ABSTRACT_ME(RegexInterface, __toString, ai_RegexInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_regex_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/TimestampInterface.c
@@ -26,17 +26,20 @@
 zend_class_entry* php_phongo_timestamp_interface_ce;
 
 /* {{{ MongoDB\BSON\TimestampInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_TimestampInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_TimestampInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_timestamp_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(TimestampInterface, getIncrement, ai_TimestampInterface_void)
 	ZEND_ABSTRACT_ME(TimestampInterface, getTimestamp, ai_TimestampInterface_void)
-	ZEND_ABSTRACT_ME(TimestampInterface, __toString, ai_TimestampInterface_void)
+	ZEND_ABSTRACT_ME(TimestampInterface, __toString, ai_TimestampInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_timestamp_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

src/BSON/UTCDateTimeInterface.c
@@ -26,16 +26,19 @@
 zend_class_entry* php_phongo_utcdatetime_interface_ce;
 
 /* {{{ MongoDB\BSON\UTCDateTimeInterface function entries */
+/* clang-format off */
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_UTCDateTimeInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_UTCDateTimeInterface_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_utcdatetime_interface_me[] = {
-	/* clang-format off */
 	ZEND_ABSTRACT_ME(UTCDateTimeInterface, toDateTime, ai_UTCDateTimeInterface_void)
-	ZEND_ABSTRACT_ME(UTCDateTimeInterface, __toString, ai_UTCDateTimeInterface_void)
+	ZEND_ABSTRACT_ME(UTCDateTimeInterface, __toString, ai_UTCDateTimeInterface___toString)
 	PHP_FE_END
-	/* clang-format on */
 };
+/* clang-format on */
 /* }}} */
 
 void php_phongo_utcdatetime_interface_init_ce(INIT_FUNC_ARGS) /* {{{ */

tests/session/session-advanceOperationTime-003.phpt
@@ -21,7 +21,7 @@ class MyTimestamp implements MongoDB\BSON\TimestampInterface
         return 1234;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
         return sprintf('[%d:%d]', $this->getIncrement(), $this->getTimestamp());
     }

commit 5668b1a5ba0d6c75afa6ffe80c3b202856b7c3b2

PHPC-2004: Use tentative return types for BSON interface toString methods
This should avoid the BC break introduced in 6c82ec8b52fad33c3e9444ae8bd2c27a729c4688. PHP 8.1 will automatically add return type info for Stringable, which means the ReturnTypeWillChange attribute should not be necessary on userland classes.

src/BSON/BinaryInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_binary_interface_ce;
 
 /* {{{ MongoDB\BSON\BinaryInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_BinaryInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_BinaryInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_BinaryInterface_void, 0, 0, 0)

src/BSON/Decimal128Interface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_decimal128_interface_ce;
 
 /* {{{ MongoDB\BSON\Decimal128Interface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_Decimal128Interface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_Decimal128Interface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_phongo_decimal128_interface_me[] = {

src/BSON/JavascriptInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_javascript_interface_ce;
 
 /* {{{ MongoDB\BSON\JavascriptInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_JavascriptInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_JavascriptInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_JavascriptInterface_void, 0, 0, 0)

src/BSON/ObjectIdInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_objectid_interface_ce;
 
 /* {{{ MongoDB\BSON\ObjectIdInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_ObjectIdInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_ObjectIdInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_ObjectIdInterface_void, 0, 0, 0)

src/BSON/RegexInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_regex_interface_ce;
 
 /* {{{ MongoDB\BSON\RegexInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_RegexInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_RegexInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_RegexInterface_void, 0, 0, 0)

src/BSON/TimestampInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_timestamp_interface_ce;
 
 /* {{{ MongoDB\BSON\TimestampInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_TimestampInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_TimestampInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_TimestampInterface_void, 0, 0, 0)

src/BSON/UTCDateTimeInterface.c
@@ -27,7 +27,7 @@ zend_class_entry* php_phongo_utcdatetime_interface_ce;
 
 /* {{{ MongoDB\BSON\UTCDateTimeInterface function entries */
 /* clang-format off */
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(ai_UTCDateTimeInterface___toString, 0, 0, IS_STRING, 0)
+ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(ai_UTCDateTimeInterface___toString, 0, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(ai_UTCDateTimeInterface_void, 0, 0, 0)

tests/session/session-advanceOperationTime-003.phpt
@@ -21,7 +21,7 @@ class MyTimestamp implements MongoDB\BSON\TimestampInterface
         return 1234;
     }
 
-    public function __toString(): string
+    public function __toString()
     {
         return sprintf('[%d:%d]', $this->getIncrement(), $this->getTimestamp());
     }

tests/session/session-advanceOperationTime_error-001.phpt
@@ -38,7 +38,7 @@ class MyTimestamp implements MongoDB\BSON\TimestampInterface
         return 1234;
     }
 
-    public function __toString(): string
+    public function __toString()
     {
         return sprintf('[%d:%d]', $this->getIncrement(), $this->getTimestamp());
     }