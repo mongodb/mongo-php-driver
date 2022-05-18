b73afcd482fb9582da7cf0cc0a62b446a93e19bb
Jeremy Mikola <jmikola@gmail.com>
2022-05-18 17:12:52 -0400

    PHPC-2053: Remove mongodb.mock_service_id INI option
    Note: this was only used for internal testing and should not be considered a BC break.

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -6,13 +6,11 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #   MONGODB_URI     Set the suggested connection MONGODB_URI (including credentials and topology info)
 #   TESTS           Optional TESTS environment variable for run-tests.php
 #   API_VERSION     Optional API_VERSION environment variable for run-tests.php
-#   MOCK_SERVICE_ID Set to "1" to enable service ID mocking for load balancers. Defaults to empty string.
 
 SSL=${SSL:-nossl}
 MONGODB_URI=${MONGODB_URI:-}
 TESTS=${TESTS:-}
 API_VERSION=${API_VERSION:-}
-MOCK_SERVICE_ID=${MOCK_SERVICE_ID:-}
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 [ -z "$MARCH" ] && MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
@@ -35,7 +33,7 @@ echo "Running tests with URI: $MONGODB_URI"
 # Run the tests, and store the results in a junit result file
 case "$OS" in
    *)
-      MOCK_SERVICE_ID="${MOCK_SERVICE_ID}" API_VERSION="${API_VERSION}" TEST_PHP_JUNIT="${PROJECT_DIRECTORY}/test-results.xml" TEST_PHP_ARGS="-q -x --show-diff -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP" make test TESTS=$TESTS
+      API_VERSION="${API_VERSION}" TEST_PHP_JUNIT="${PROJECT_DIRECTORY}/test-results.xml" TEST_PHP_ARGS="-q -x --show-diff -g FAIL,XFAIL,BORK,WARN,LEAK,SKIP" make test TESTS=$TESTS
       ;;
 esac
 
