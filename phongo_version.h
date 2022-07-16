1a3e32951ec1c0c29a693ff3c8fc7806828bfc36
Jeremy Mikola <jmikola@gmail.com>
2022-07-16 14:02:00 -0700

    Package 1.14.0

--- phongo_version.h
+++ phongo_version.h
@@ -22,9 +22,9 @@
  * publishing a release. */
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.14.0-dev"
-#define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,14,0,2
+#define PHP_MONGODB_VERSION "1.14.0"
+#define PHP_MONGODB_STABILITY "stable"
+#define PHP_MONGODB_VERSION_DESC 1,14,0,3
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
