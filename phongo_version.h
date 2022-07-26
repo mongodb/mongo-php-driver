da4d0716cb5957c3310d11597d6d70de57a4eec3
Andreas Braun <git@alcaeus.org>
2022-07-26 12:03:32 +0200

    Master is now 1.15-dev

--- phongo_version.h
+++ phongo_version.h
@@ -22,9 +22,9 @@
  * publishing a release. */
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.14.1-dev"
+#define PHP_MONGODB_VERSION "1.15.0-dev"
 #define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,14,1,0
+#define PHP_MONGODB_VERSION_DESC 1,15,0,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
