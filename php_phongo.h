0abee61964224b601d7d8d217176757d691967b4
Andreas Braun <git@alcaeus.org>
2022-08-02 09:31:24 +0200

    Make disabled constructor/wakeup methods static

--- php_phongo.h
+++ php_phongo.h
@@ -110,14 +110,14 @@ zend_object_handlers* phongo_get_std_object_handlers(void);
 	} while (0)
 
 #define PHONGO_DISABLED_CONSTRUCTOR(classname)                                         \
-	PHP_METHOD(classname, __construct)                                                 \
+	static PHP_METHOD(classname, __construct)                                          \
 	{                                                                                  \
 		PHONGO_PARSE_PARAMETERS_NONE();                                                \
 		phongo_throw_exception(PHONGO_ERROR_RUNTIME, "Accessing private constructor"); \
 	}
 
 #define PHONGO_DISABLED_WAKEUP(classname)                                                             \
-	PHP_METHOD(classname, __wakeup)                                                                   \
+	static PHP_METHOD(classname, __wakeup)                                                            \
 	{                                                                                                 \
 		PHONGO_PARSE_PARAMETERS_NONE();                                                               \
 		phongo_throw_exception(PHONGO_ERROR_RUNTIME, "MongoDB\\Driver objects cannot be serialized"); \
