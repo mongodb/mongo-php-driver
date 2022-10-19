mongodb/mongo-php-driver

commit b581f2a3e43d7f8d22afaaebbb1ab2acb8636209

PHPC-1706: Don't try linking against libresolv on AIX (#1172)
On AIX, the resolver functions are in libc, so trying libresolv
will cause the checks to fail. The behaviour should be instead
like AC_SEARCH_LIBS.

This hardcodes a check not to use libresolv on AIX. Better than
nothing, but it may not compensate for other systems without a
libresolv.

scripts/autotools/libmongoc/CheckResolv.m4
@@ -1,10 +1,13 @@
 dnl Disable Windows DNSAPI
 AC_SUBST(MONGOC_HAVE_DNSAPI, 0)
 
-found_resolv="no"
+need_libresolv="no"
 
 old_LIBS="$LIBS"
-LIBS="$LIBS -lresolv"
+dnl On AIX, resolv functions are in libc.
+if test "x$os_aix" = "xno"; then
+  LIBS="$LIBS -lresolv"
+fi
 
 dnl Thread-safe DNS query function for _mongoc_client_get_srv.
 dnl Could be a macro, not a function, so check with AC_LINK_IFELSE.
@@ -24,7 +27,6 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
    AC_MSG_RESULT([yes])
    AC_SUBST(MONGOC_HAVE_RES_SEARCH, 0)
    AC_SUBST(MONGOC_HAVE_RES_NSEARCH, 1)
-   found_resolv="yes"
 
    dnl We have res_nsearch. Call res_ndestroy (BSD/Mac) or res_nclose (Linux)?
    AC_MSG_CHECKING([for res_ndestroy])
@@ -80,7 +82,9 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
    ]])], [
       AC_MSG_RESULT([yes])
       AC_SUBST(MONGOC_HAVE_RES_SEARCH, 1)
-      found_resolv="yes"
+      if test "x$os_aix" = "xno"; then
+        need_libresolv="yes"
+      fi
    ], [
       AC_MSG_RESULT([no])
       AC_SUBST(MONGOC_HAVE_RES_SEARCH, 0)
@@ -89,6 +93,6 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
 
 LIBS="$old_LIBS"
 
-AS_IF([test "$found_resolv" = "yes"],[
+AS_IF([test "$need_libresolv" = "yes"],[
   PHP_ADD_LIBRARY([resolv],,[MONGODB_SHARED_LIBADD])
 ])

scripts/autotools/CheckHost.m4
@@ -9,6 +9,7 @@ os_linux=no
 os_solaris=no
 os_darwin=no
 os_gnu=no
+os_aix=no
 
 case "$host" in
     *-mingw*|*-*-cygwin*)
@@ -42,6 +43,10 @@ case "$host" in
         ;;
     *-*-darwin*)
         os_darwin=yes
+        TARGET_OS=unix
+        ;;
+    *-*-aix*|*-*-os400*)
+        os_aix=yes
         TARGET_OS=unix
         ;;
     gnu*|k*bsd*-gnu*)

commit f3a01ab1a81b705b3f78d2d3bb6ac7aad88bbca1

PHPC-2152: Link libresolv when detecting res_nsearch
This fixes a bug inadvertently introduced in b581f2a3e43d7f8d22afaaebbb1ab2acb8636209.

scripts/autotools/libmongoc/CheckResolv.m4
@@ -1,11 +1,12 @@
 dnl Disable Windows DNSAPI
 AC_SUBST(MONGOC_HAVE_DNSAPI, 0)
 
-need_libresolv="no"
+found_resolv="no"
 
 old_LIBS="$LIBS"
-dnl On AIX, resolv functions are in libc.
-if test "x$os_aix" = "xno"; then
+
+dnl Link libresolv for detection (note: on AIX, resolv functions are in libc)
+if test "$os_aix" != "yes"; then
   LIBS="$LIBS -lresolv"
 fi
 
@@ -27,6 +28,8 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
    AC_MSG_RESULT([yes])
    AC_SUBST(MONGOC_HAVE_RES_SEARCH, 0)
    AC_SUBST(MONGOC_HAVE_RES_NSEARCH, 1)
+
+   found_resolv="yes"
 
    dnl We have res_nsearch. Call res_ndestroy (BSD/Mac) or res_nclose (Linux)?
    AC_MSG_CHECKING([for res_ndestroy])
@@ -82,9 +85,8 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
    ]])], [
       AC_MSG_RESULT([yes])
       AC_SUBST(MONGOC_HAVE_RES_SEARCH, 1)
-      if test "x$os_aix" = "xno"; then
-        need_libresolv="yes"
-      fi
+
+      found_resolv="yes"
    ], [
       AC_MSG_RESULT([no])
       AC_SUBST(MONGOC_HAVE_RES_SEARCH, 0)
@@ -93,6 +95,7 @@ AC_LINK_IFELSE([AC_LANG_PROGRAM([[
 
 LIBS="$old_LIBS"
 
-AS_IF([test "$need_libresolv" = "yes"],[
+dnl Link libresolv if needed (note: on AIX, resolv functions are in libc)
+AS_IF([test "$found_resolv" = "yes" -a "$os_aix" != "yes"],[
   PHP_ADD_LIBRARY([resolv],,[MONGODB_SHARED_LIBADD])
 ])