a86a162956184ef5958b5cac97c61464c74424c6
Jeremy Mikola <jmikola@gmail.com>
2022-11-23 11:12:56 +0800

    Master is now 1.16-dev

--- phongo_version.h
+++ phongo_version.h
@@ -22,9 +22,9 @@
  * publishing a release. */
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.15.1-dev"
+#define PHP_MONGODB_VERSION "1.16.0-dev"
 #define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,15,1,0
+#define PHP_MONGODB_VERSION_DESC 1,16,0,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
