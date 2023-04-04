8c9eded0b1c9511ad8c8d65b0f94399e0c287b2d
Jeremy Mikola <jmikola@gmail.com>
2023-04-04 14:11:50 +0800

    PHPC-2195: Use less ambiguous path in mongocrypt.h compat include
    Also add an ifndef block so we're not just relying on the actual mongocrypt.h include.

--- src/libmongocrypt-compat/mongocrypt/mongocrypt.h
+++ src/libmongocrypt-compat/mongocrypt/mongocrypt.h
@@ -1,1 +1,12 @@
-#include "../libmongocrypt/src/mongocrypt.h"
+#ifndef PHONGO_COMPAT_MONGOCRYPT_H
+#define PHONGO_COMPAT_MONGOCRYPT_H
+
+/* This header forwards includes for "mongocrypt/mongocrypt.h" to the desired
+ * header in the bundled libmongocrypt submodule. This is used when compiling
+ * bundled versions of libmongoc and libmongocrypt and is necessary because
+ * libmongocrypt's source directory is not named "mongocrypt/".
+ *
+ * This path is relative to the PHPC project directory to avoid ambiguity. */
+#include "src/libmongocrypt/src/mongocrypt.h"
+
+#endif /* PHONGO_COMPAT_MONGOCRYPT_H */
