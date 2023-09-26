6607c0f167c0325de61e3b238be4d6103f44c886
Andreas Braun <git@alcaeus.org>
2023-09-26 09:30:21 +0200

    Refactor generation script

--- .evergreen/config/generate-config.php
+++ .evergreen/config/generate-config.php
@@ -1,40 +1,50 @@
 #!/bin/env php
 <?php
 
-$buildPhpVersions = [
-    '8.2', '8.1', '8.0', '7.4',
-];
-$libmongocBuildPhpVersions = ['8.2'];
+// Supported PHP versions. Add new versions to the beginning of the list
+$supportedPhpVersions = ['8.2', '8.1', '8.0', '7.4'];
 
-$localServerVersions = [
+// Supported MongoDB versions. Add new versions after "rapid"
+$supportedMongoDBVersions = [
     'latest', 'rapid',
     '7.0', '6.0', '5.0',
     '4.4', '4.2', '4.0',
     '3.6',
 ];
-$loadBalancedServerVersions = [
-    'latest', 'rapid',
-    '7.0', '6.0', '5.0',
-];
-$ocspServerVersions = [
-    'latest', 'rapid',
-    '7.0', '6.0', '5.0',
-    '4.4',
-];
-$requireApiServerVersions = [
-    'latest', 'rapid',
-    '7.0', '6.0', '5.0',
-];
-$skipCryptSharedServerVersions = [
-    'latest', 'rapid',
-    '7.0', '6.0',
-];
+
+// Only test the latest PHP version for libmongoc
+$libmongocBuildPhpVersions = array_slice($supportedPhpVersions, 0, 1);
+
+// Server versions
+$localServerVersions = $supportedMongoDBVersions;
+$loadBalancedServerVersions = array_filter(
+    $supportedMongoDBVersions,
+    // Load balanced supports MongoDB 5.0+
+    fn (string $version): bool => in_array($version, ['latest', 'rapid']) || version_compare($version, '5.0', '>='),
+);
+$ocspServerVersions = array_filter(
+    $supportedMongoDBVersions,
+    // OCSP supports MongoDB 4.4+
+    fn (string $version): bool => in_array($version, ['latest', 'rapid']) || version_compare($version, '4.4', '>='),
+);
+$requireApiServerVersions = array_filter(
+    $supportedMongoDBVersions,
+    // requireApiVersion supports MongoDB 5.0+
+    fn (string $version): bool => in_array($version, ['latest', 'rapid']) || version_compare($version, '5.0', '>='),
+);
+$skipCryptSharedServerVersions = array_filter(
+    $supportedMongoDBVersions,
+    // crypt_shared is available starting with MongoDB 6.0
+    fn (string $version): bool => in_array($version, ['latest', 'rapid']) || version_compare($version, '6.0', '>='),
+);
 
 $allFiles = [];
 
-$allFiles[] = generateConfigs('build', 'phpVersion', '_template-build-php.yml', 'build-php-%s', $buildPhpVersions);
+// Build tasks
+$allFiles[] = generateConfigs('build', 'phpVersion', '_template-build-php.yml', 'build-php-%s', $supportedPhpVersions);
 $allFiles[] = generateConfigs('build', 'phpVersion', '_template-build-libmongoc.yml', 'build-libmongoc-%s', $libmongocBuildPhpVersions);
 
+// Test tasks
 $allFiles[] = generateConfigs('test', 'mongodbVersion', '_template-local.yml', 'local-%s', $localServerVersions);
 $allFiles[] = generateConfigs('test', 'mongodbVersion', '_template-load-balanced.yml', 'load-balanced-%s', $loadBalancedServerVersions);
 $allFiles[] = generateConfigs('test', 'mongodbVersion', '_template-ocsp.yml', 'ocsp-%s', $ocspServerVersions);
