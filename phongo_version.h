b07f62617783bdf7dd8f3eb09a1a01aa64a1cdb2
Jeremy Mikola <jmikola@gmail.com>
2024-03-27 11:41:36 -0400

    Package 1.18.0

--- phongo_version.h
+++ phongo_version.h
@@ -23,8 +23,8 @@
 
 /* clang-format off */
 #define PHP_MONGODB_VERSION "1.18.0"
-#define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,18,0,0
+#define PHP_MONGODB_STABILITY "stable"
+#define PHP_MONGODB_VERSION_DESC 1,18,0,1
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
