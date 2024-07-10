705f2aa8b50365f0e3e6ad43d487cc5b81dd0143
Andreas Braun <andreas.braun@mongodb.com>
2024-07-10 09:13:29 +0200

    PHPC-2347: Deprecate BSON functions

--- src/functions.stub.php
+++ src/functions.stub.php
@@ -3,26 +3,38 @@
 /** @generate-function-entries */
 
 namespace MongoDB\BSON {
+    /** @deprecated */
     function fromJSON(string $json): string {}
 
 #if PHP_VERSION_ID >= 80000
+    /** @deprecated */
     function fromPHP(array|object $value): string {}
 #else
-    /** @param array|object $value */
+    /**
+     * @param array|object $value
+     * @deprecated
+     */
     function fromPHP($value): string {}
 #endif
 
+    /** @deprecated */
     function toCanonicalExtendedJSON(string $bson): string {}
 
+    /** @deprecated */
     function toJSON(string $bson): string {}
 
 #if PHP_VERSION_ID >= 80000
+    /** @deprecated */
     function toPHP(string $bson, ?array $typemap = null): array|object {}
 #else
-    /** @return array|object */
+    /**
+     * @return array|object
+     * @deprecated
+     */
     function toPHP(string $bson, ?array $typemap = null) {}
 #endif
 
+    /** @deprecated */
     function toRelaxedExtendedJSON(string $bson): string {}
 }
 
