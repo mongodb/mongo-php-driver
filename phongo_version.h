15a10690c825583da4c665a09f0aadda50befede
Andreas Braun <andreas.braun@mongodb.com>
2026-02-11 09:47:51 +0100

    Master is now 2.3-dev

--- phongo_version.h
+++ phongo_version.h
@@ -22,9 +22,9 @@
  * publishing a release. */
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "2.2.1dev"
+#define PHP_MONGODB_VERSION "2.3.0dev"
 #define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 2,2,1,0
+#define PHP_MONGODB_VERSION_DESC 2,3,0,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
