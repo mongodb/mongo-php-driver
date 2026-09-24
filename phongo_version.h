a5ae5082abe51609509f9259711b54a630727dde
Kevin Albertson <kevin.albertson@mongodb.com>
2026-09-24 10:50:21 -0400

    Revert "Back to -dev"
    This reverts commit 137fbaad0eede1ea5e9023ba47dda0552f619d6a.

--- phongo_version.h
+++ phongo_version.h
@@ -22,9 +22,9 @@
  * publishing a release. */
 
 /* clang-format off */
-#define PHP_MONGODB_VERSION "1.21.11dev"
-#define PHP_MONGODB_STABILITY "devel"
-#define PHP_MONGODB_VERSION_DESC 1,21,11,0
+#define PHP_MONGODB_VERSION "1.21.10"
+#define PHP_MONGODB_STABILITY "stable"
+#define PHP_MONGODB_VERSION_DESC 1,21,10,0
 /* clang-format on */
 
 #endif /* PHONGO_VERSION_H */
